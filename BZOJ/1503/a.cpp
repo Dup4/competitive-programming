89f0685170f43705ba6d26471cf75993d03cee2d
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-04-09 23:30:47 +0800

    update

---
+++ BZOJ/1503/a.cpp
@@ -0,0 +1,213 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 300010
+#define INF 0x3f3f3f3f3f3f3f3f
+int n; ll m;
+struct Splay
+{
+	int root, tot;
+	struct node
+	{
+		int son[2]; ll val;
+		int cnt, fa, sze;
+		node() {}
+	}t[N];
+	//初始化
+	void init() { root = tot = 0; }
+	//更新子树大小
+	void pushup(int u) 
+	{
+		int ls = t[u].son[0], rs = t[u].son[1];
+		t[u].sze = t[ls].sze + t[rs].sze + t[u].cnt;
+	}
+	/*
+		旋转操作
+		假设z->y->x
+		X变到Y原来的位置
+		我们令X为Y的第p个儿子，那么q = p ^ 1
+		Y变成了 X的第q个儿子
+		Y的第q个儿子不变 X的第p个儿子不变
+		X的第q个儿子变成了Y的第p个儿子
+	*/
+	void rotate(int x)
+	{
+		int y = t[x].fa;
+		int z = t[y].fa;
+		int k = (t[y].son[1] == x); 		//判断x是其父亲的哪个儿子
+		t[z].son[t[z].son[1] == y] = x; 	//X变为Y原来的位置 
+		t[x].fa = z;						
+		t[y].son[k] = t[x].son[k ^ 1]; 		//X的第q个儿子变成Y的第p个儿子
+		t[t[x].son[k ^ 1]].fa = y; 			
+		t[x].son[k ^ 1] = y;				//Y变成X的第q个儿子
+		t[y].fa = x;
+		pushup(y); pushup(x); 				//先更新儿子的子树大小，再更新当前点的子树大小 
+	}
+	/*
+	   splay操作
+	   第一种：X和Y分别是Y和Z的同一个儿子
+	   先旋转Y再旋转X
+	   第二种：X和Y分别是Y和Z不同的儿子
+	   X旋转两次
+	*/
+	void splay(int x, int tar)  //旋转到tar的儿子处 
+	{
+		while (t[x].fa != tar) 
+		{
+			int y = t[x].fa;
+			int z = t[y].fa;
+			if (z != tar)     // 如果Z不是目标父亲
+				(t[y].son[0] == x) ^ (t[z].son[0] == y) ? rotate(x) : rotate(y); //判断是哪一种情况  
+			rotate(x);        //最后都是旋转X
+		}
+		if (tar == 0)
+			root = x;
+	}
+	/*
+		插入操作
+		递归查找，splay本质是二叉搜索树
+	    如果存在，直接计数	
+		如果不存在，找到父节点位置新增节点
+	*/
+	void insert(ll x)
+	{
+		int u = root, fa = 0;
+		while (u && t[u].val != x) 
+		{
+			fa = u;
+			u = t[u].son[x > t[u].val]; 
+		}
+		if (u) ++t[u].cnt;
+		else
+		{
+			u = ++tot;
+			if (fa)
+				t[fa].son[x > t[fa].val] = u; 
+			t[u].son[0] = t[u].son[1] = 0;
+			t[u].fa = fa;
+			t[u].val = x;
+			t[u].cnt = t[u].sze = 1;
+		}
+		splay(u, 0);
+	}
+	/*
+		递归查找，并且讲找到的节点旋转到根
+	    方便下一步的操作
+		但是，要注意如果找不到，则会找到它的前驱或者后继
+		要进行特判	
+	*/
+	void find(ll x)
+	{
+		int u = root;
+		if (!u) return;
+		while (t[u].son[x > t[u].val] && x != t[u].val)
+			u = t[u].son[x > t[u].val];
+		splay(u, 0);
+	}
+	/*
+		前驱(0)，后继(1)
+		前驱：小于X并且最大的值
+		后继：大于X并且最小的值
+	*/
+	int Next(ll x, int f)
+	{
+		find(x);
+		int u = root;                    //根节点，此时X的父节点(存在的话)就是根节点
+		if (f && t[u].val > x) return u; //如果根节点的值>X并且要找后继
+		if (!f && t[u].val < x) return u;//如果根节点的值<X并且要找前驱
+		u = t[u].son[f];
+		while (t[u].son[f ^ 1]) u = t[u].son[f ^ 1]; //在对应子树往反方向走
+		return u;
+	}
+	/*
+		找到前驱，splay到根节点
+	    找到后继，splay到前驱的底下
+		那么所有小于X的都在根节点以及它的左子树中
+		那么所有大于X的都在根节点的右儿子以及它的右子树中
+		那么如果X存在，那么它必然在根节点的右儿子的左儿子
+		并且左儿子是叶子节点	
+	*/
+	void Delete(ll x) 
+	{
+		int last = Next(x, 0);
+		int nx = Next(x, 1);
+		splay(last, 0); splay(nx, last); 
+		int del = t[nx].son[0];
+		if (t[del].cnt > 1) 
+		{
+			--t[del].cnt;
+			--t[del].sze; 
+			//splay(nx, 0);
+			splay(del, 0); 
+		}
+		else
+		{
+			t[nx].son[0] = 0;
+			splay(nx, 0);
+		}
+	}
+	/*
+		左子树子树大小>=K,递归左子树
+	   	否则K- 左子树大小，递归右子树	
+	*/
+	ll kth(int k)
+	{
+		int u = root;
+		if (t[u].sze < k)
+			return 0;
+		while (1)
+		{
+			int y = t[u].son[0];
+			if (k > t[y].sze + t[u].cnt)
+			{
+				k -= t[y].sze + t[u].cnt;
+				u = t[u].son[1];
+			}
+			else
+			{
+				if (t[y].sze >= k)
+					u = y;
+				else
+					return t[u].val;
+			}
+		}
+	}
+}sp;
+
+int main()
+{
+	scanf("%d%lld", &n, &m);
+	sp.init();
+	sp.insert(INF);
+	sp.insert(-INF);
+	char op[10]; int cnt = 0, sze = 2;  ll x, lazy = 0;
+	while (n--)
+	{
+		scanf("%s%lld", op, &x);
+		switch(op[0])  
+		{
+			case 'I' :
+				if (x >= m)
+					sp.insert(x - lazy), ++sze; 
+				break;
+			case 'A' :
+				lazy += x;
+				break;
+			case 'S' :
+				lazy -= x;
+				while (sp.t[sp.root].sze >= 3 && sp.kth(2) + lazy < m)  
+					sp.Delete(sp.kth(2)), ++cnt, --sze;  
+				break;
+			case 'F' :
+				sze = sp.t[sp.root].sze;
+				if (sze - x < 2)  
+					puts("-1");
+				else
+					printf("%lld\n", sp.kth(sze - x) + lazy);
+				break;
+		}
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
---
+++ BZOJ/1503/b.cpp
@@ -0,0 +1,177 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define INF 1000000000
+#define MAX 110000
+int root;
+struct Node
+{
+    int ch[2];
+    int val;
+    int ff;
+    int cnt;
+    int size;
+}t[MAX];
+int tot,n,mm,ans,add;
+inline void pushup(int x)
+{
+    t[x].size=t[t[x].ch[0]].size+t[t[x].ch[1]].size+t[x].cnt;
+}
+inline void rotate(int x)
+{
+    int y=t[x].ff;
+    int z=t[y].ff;
+    int k=(x==t[y].ch[1]);
+    t[z].ch[y==t[z].ch[1]]=x;
+    t[x].ff=z;
+    t[y].ch[k]=t[x].ch[k^1];   t[t[x].ch[k^1]].ff=y;
+    t[x].ch[k^1]=y;            t[y].ff=x;
+    pushup(y);pushup(x);
+}
+inline void splay(int x,int goal)
+{
+    while(t[x].ff!=goal)
+    {
+        int y=t[x].ff;
+        int z=t[y].ff;
+        if(z!=goal)
+            (t[z].ch[0]==y)^(t[y].ch[0]==x)?rotate(x):rotate(y);
+        rotate(x);
+    }
+    if(goal==0)root=x;
+}
+inline void insert(int x)
+{
+    int u=root,ff=0;
+    while(u&&t[u].val!=x)
+    {
+        ff=u;
+        u=t[u].ch[x>t[u].val];
+    }
+    if(u)t[u].cnt++;
+    else
+    {
+        u=++tot;
+        if(ff)t[ff].ch[x>t[ff].val]=u;
+        t[u].ch[0]=t[u].ch[1]=0;
+        t[u].val=x;
+        t[u].cnt=1;
+        t[u].size=1;
+        t[u].ff=ff;
+    }
+    splay(u,0);
+}
+inline void find(int x)
+{
+    int u=root;
+    if(!u)return;
+    while(t[u].ch[x>t[u].val]&&t[u].val!=x)
+        u=t[u].ch[x>t[u].val];
+    splay(u,0);
+}
+inline int Next(int x,int f)
+{
+    find(x);
+    int u=root;
+    if(t[u].val>=x&&f)return u;
+    if(t[u].val<=x&&!f)return u;
+    u=t[u].ch[f];
+    while(t[u].ch[f^1])u=t[u].ch[f^1];
+    return u;
+}
+inline int kth(int x)
+{
+    int u=root;
+    //if(t[u].size<x||x<=0)return -INF;
+    while(233)
+    {
+        int y=t[u].ch[0];
+        if(t[y].size+t[u].cnt<x)
+        {
+            x-=t[y].size+t[u].cnt;
+            u=t[u].ch[1];
+        }
+        else
+            if(t[y].size>=x)
+                u=y;
+            else
+                return t[u].val;
+    }
+}
+
+void Delete(int x) 
+{
+	int last = Next(x, 0);
+	int nx = Next(x, 1);
+	splay(last, 0); splay(nx, last); 
+	int del = t[nx].ch[0];
+	if (t[del].cnt > 1) 
+	{
+		--t[del].cnt;
+		splay(del, 0);
+	}
+	else
+	{
+		t[nx].ch[0] = 0;
+		//splay(nx, 0);
+		pushup(nx); pushup(last);
+	}
+}
+int main()
+{
+	insert(-INF); 
+    insert(+INF);
+    scanf("%d%d",&n,&mm);
+    int peo=0;
+    while(n--)
+    {
+        int x;char ch[3];
+        scanf("%s%d",ch,&x);
+        if(ch[0]=='I')
+        {
+            if(x<mm);
+            else
+            {
+                insert(x-add);
+                peo++;
+            }
+        }
+        if(ch[0]=='A')
+            add+=x;
+        if(ch[0]=='S')
+        {
+            add-=x;
+			while (peo >= 1 && kth(2) < mm - add)  
+			{
+				Delete(kth(2)), ++ans, --peo;
+			}
+			/*	
+            int gg=Next(mm-add,1);
+            splay(gg,0);
+            ans+=t[t[root].ch[0]].size;
+            peo-=t[t[root].ch[0]].size;
+            t[t[root].ch[0]].size=t[t[root].ch[0]].cnt=0;
+            t[root].ch[0]=0;t[0].size=0;
+            pushup(root);
+			*/
+        }
+        if(ch[0]=='F')
+        {
+			if (peo - x < 0)
+				puts("-1");
+			else
+			{
+				int gg = kth(2);
+            	printf("%d\n",gg+add);
+			}
+        }
+    }
+    printf("%d\n",ans);
+    return 0;
+}
