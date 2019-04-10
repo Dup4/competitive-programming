3f92d9e08bec0a3868c0f85ae337f13c73779fbe
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-04-10 23:31:23 +0800

    up

--- BZOJ/2694/a.cpp
+++ BZOJ/2694/a.cpp
@@ -5,51 +5,55 @@ using namespace std;
 #define N 4000010
 const ll p = 1 << 30;
 int mu[N], prime[N];
-ll f[N];
+int f[N];
 bool check[N];
 void init()
 {
 	memset(check, 0, sizeof check);
 	prime[0] = 0; mu[1] = 1;
+	f[1] = 1; 
 	for (int i = 2; i < N; ++i)
 	{
-		if (!check[i])
+		if (!check[i]) 
 		{
 			prime[++prime[0]] = i;
 			mu[i] = -1;
+			f[i] = i - 1ll * i * i;
 		}
 		for (int j = 1; j <= prime[0]; ++j)
 		{
-			if (1ll * i * prime[j] >= N) break;
+			if (1ll * i * prime[j] >= N) break; 
 			check[i * prime[j]] = 1;
 			if (i % prime[j] == 0)
 			{
 				mu[i * prime[j]] = 0;
+				if ((i / prime[j]) % prime[j])
+					f[i * prime[j]] = f[i / prime[j]] * -prime[j] * prime[j] * prime[j];
+				else
+					f[i * prime[j]] = 0; 
 				break;
 			}
 			else
+			{
 				mu[i * prime[j]] = -mu[i];
+				f[i * prime[j]] = f[i] * f[prime[j]];
+			}
 		}
 	}
-	memset(f, 0, sizeof f);
-	for (int i = 1; i < N; ++i)
-		for (int j = i; j < N; j += i) if (mu[i])
-			f[j] += i * mu[j / i];
 	for (int i = 2; i < N; ++i)
-		f[i] = (f[i] + f[i - 1]); 
-	
+		f[i] += f[i - 1];
 }
 
-ll get(int x) { return (1ll * x * (x + 1) / 2) % p; }
+int get(int x) { return x * (x + 1) / 2; }
 
-ll work(int a, int b)
+int work(int a, int b)
 {
 	if (a > b) swap(a, b);
-	ll res = 0;
+	int res = 0;
 	for (int i = 1, j; i <= a; i = j + 1)  
 	{
 		j = min(a / (a / i), b / (b / i)); 
-		res = (res + get(a / i)* get(b / i) % p * (f[j] - f[i - 1] + p) % p + p) % p;
+		res += get(a / i)* get(b / i) * (f[j] - f[i - 1]);
 	}
 	return res; 
 }
@@ -61,7 +65,7 @@ int main()
 	while (T--)
 	{
 		int n, m; scanf("%d%d", &n, &m);
-		printf("%lld\n", work(n, m));
+		printf("%lld\n", (work(n, m) % p + p) % p);
 	}
 	return 0;
 }
