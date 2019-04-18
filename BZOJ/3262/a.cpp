56c7897d07760bd24cdc241b70b9843f1c864052
[Dup4] <[panlvzhi@foxmail.com]>
2019-04-18 17:27:49 +0800

    up

--- BZOJ/3262/a.cpp
+++ BZOJ/3262/a.cpp
@@ -43,7 +43,7 @@ struct SPLAY {
 		int z = t[y].fa;
 		int k = t[y].son[1] == x;
 		t[x].fa = z; t[z].son[t[z].son[1] == y] = x;
-		t[y].son[k] = t[x].son[k ^ 1]; t[x].son[k ^ 1] = y;
+		t[y].son[k] = t[x].son[k ^ 1]; t[t[x].son[k ^ 1]].fa = y;
 		t[y].fa = x; t[x].son[k ^ 1] = y;
 		pushup(y); pushup(x);
 	}
@@ -60,26 +60,28 @@ struct SPLAY {
 		if (tar == 0) {
 			rt = x; 
 		}
-	}
+	} 
 	void insert(int &rt, int x, int cnt) {
-		int u = rt, fa = 0;
+		int u = rt, fa = 0;   
 		while (u && t[u].val != x) {
 			fa = u;
-			u = t[u].son[x > t[u].val];
+			u = t[u].son[x > t[u].val];  
 		}
 		if (u) {
 			t[u].cnt += cnt;
+			t[u].sze += cnt; 
 		} else {
 			u = ++tot;
 			if (fa) {
-				t[fa].son[x > t[u].val] = u;
+				t[fa].son[x > t[fa].val] = u;
 			}
+			t[u].val = x;
 			t[u].son[0] = t[u].son[1] = 0;
 			t[u].cnt = t[u].sze = cnt;
 			t[u].fa = fa;
 		}
-		pushup(x);
-		splay(rt, x, 0);
+		pushup(u);
+		splay(rt, u, 0);
 	}
 	int get(int &rt, int x) {
 		int u = rt;
@@ -88,29 +90,29 @@ struct SPLAY {
 			int ls = t[u].son[0];
 			int rs = t[u].son[1];
 			if (x < t[u].val) {
-				u = ls;
-			} else if (x == t[u].val) {
-				res += t[u].cnt; + t[ls].sze;
-				break;
-			} else { 
-				res += t[ls].sze + t[u].cnt;
+				u = ls;  
+			} else {
+				res += t[u].cnt + t[ls].sze;
+				if (x == t[u].val) {
+					break;
+				}
 				u = rs;
 			}
 		}
-		return res;
+		return res;  
 	}
 }sp;
 
 struct BIT {
 	void update(int x, int c, int v) {
 		for (; x < N; x += x & -x) {
-			sp.insert(x, c, v);
+			sp.insert(sp.root[x], c, v);
 		}
 	}
 	int query(int x, int c) {
 		int res = 0;
 		for (; x > 0; x -= x & -x) {
-			res += sp.get(sp.root[x], c - 1); 
+			res += sp.get(sp.root[x], c); 
 		}
 		return res;
 	}
@@ -128,11 +130,10 @@ int main() {
 		sort(qrr + 1, qrr + 1 + n);
 		for (int i = 1, c = 0; i <= n; ++i) {
 			++c;
-			if (i == n || !(qrr[i] == qrr[i - 1])) {
-				bit.update(qrr[i].b, qrr[i].c, c);
-				res[bit.query(qrr[i].b, qrr[i].c) + c - 1] += c;	
-				c = 0; 	
-			}
+		    if (i != n && qrr[i] == qrr[i + 1]) continue;	
+			res[bit.query(qrr[i].b, qrr[i].c) + c - 1] += c;	
+			bit.update(qrr[i].b, qrr[i].c, c);
+			c = 0; 	
 		}
 		for (int i = 0; i < n; ++i) {
 			printf("%d\n", res[i]);
