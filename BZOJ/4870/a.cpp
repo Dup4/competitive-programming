e22c3d594e4e1d6588dcfc398b1fa364a6f10624
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-04-19 09:34:53 +0800

    up

---
+++ BZOJ/4870/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 55
+ll n, p;
+int r, k;
+struct node {
+	
+};
+
+int main() {
+	while (scanf("%lld%d%d%lld", &&&&))
+	return 0;
+}
