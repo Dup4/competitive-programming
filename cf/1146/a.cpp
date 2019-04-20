d6a81ef5f2c2fd1d3b6f7ca940a2f59ea2d8e4cf
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-04-21 07:40:01 +0800

    up

---
+++ cf/1146/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 110
+char s[N];
+
+int main() {
+	while (scanf("%s", s + 1) != EOF) {
+		int cnt = 0;
+		int sze = strlen(s + 1);
+		for (int i = 1; i <= sze; ++i) {
+			cnt += s[i] == 'a';
+		}
+		for (int i = sze; i >= 1; --i) {
+			if (cnt > i / 2) {
+				printf("%d\n", i);
+				break;
+			}
+		}
+	}
+	return 0;
+}
---
+++ cf/1146/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 110
+int n;
+
+int main() {
+	int T; cin >> T;
+	while (T--) {
+		scanf("%d", &n);
+		int res = 0;
+		for (int i = 0; i < 9; ++i) {
+			vector <int> a, b;
+			for (int j = 1; j <= n; ++j) {
+				if ((j >> i) & 1) {
+					a.push_back(j);
+				} else {
+					b.push_back(j);
+				}
+			}
+			int k1 = (int)a.size(), k2 = (int)b.size();
+			if (k1 == 0 || k2 == 0) {
+				continue;
+			}
+			printf("%d %d", k1, k2);
+			fflush(stdout);
+			for (auto it : a) {
+				printf(" %d", it);
+				fflush(stdout);
+			}
+			for (auto it : b) {
+				printf(" %d", it);
+				fflush(stdout);
+			}
+			printf("\n");
+			fflush(stdout);
+			int x; scanf("%d", &x);
+			res = max(res, x);
+		}
+		printf("-1 %d\n", res);
+		fflush(stdout);
+	}
+	return 0;
+}
