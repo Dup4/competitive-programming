0a37404d842edf3ee2e5287e16e37ad505943fe9
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-05-04 22:39:15 +0800

    up

---
+++ BZOJ/4827/a.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF 0x3f3f3f3f3f3f3f3f
+const double PI = acos(-1.0);
+struct Complex {
+	double x, y;
+	Complex(double _x = 0.0, double _y = 0.0) {
+		x = _x;
+		y = _y;
+	}
+	Complex operator - (const Complex &b) const {
+		return Complex(x - b.x, y - b.y);
+	}
+	Complex operator + (const Complex &b) const {
+		return Complex(x + b.x, y + b.y);
+	}
+	Complex operator * (const Complex &b) const {
+		return Complex(x * b.x - y * b.y, x * b.y + y * b.x);
+	}
+};
+void change(Complex *y, int len) {
+	int i, j, k;
+	for (i = 1, j = len / 2; i < len - 1; ++i) {
+		if (i < j) {
+			swap(y[i], y[j]);
+		}
+		k = len / 2;
+		while (j >= k) {
+			j -= k;
+			k /= 2;
+		}
+		if (j < k) {
+			j += k;
+		}
+	}
+}
+void fft(Complex *y, int len, int on) {
+	change(y, len);
+	for (int h = 2; h <= len; h <<= 1) {
+		Complex wn(cos(-on * 2 * PI / h), sin(-on * 2 * PI / h));
+		for (int j = 0; j < len; j += h) {
+			Complex w(1, 0);
+			for (int k = j; k < j + h / 2; ++k) {
+				Complex u = y[k];
+				Complex t = w * y[k + h / 2];
+				y[k] = u + t;
+				y[k + h / 2] = u - t;
+				w = w * wn;
+			}
+		}	
+	}
+	if (on == -1) {
+		for (int i = 0; i < len; ++i) {
+			y[i].x /= len;
+		}
+	}
+}
+
+#define N 400010
+int n, m;
+Complex x1[N], x2[N];
+int a[N], b[N];
+ll sum[2][2];
+ll c[N];
+
+int main() {
+	while (scanf("%d%d", &n, &m) != EOF) {
+		memset(sum, 0, sizeof sum);
+		m = 2 * n;
+		for (int i = n; i >= 1; --i) {
+			scanf("%d", a + i);
+			sum[0][0] += a[i];
+			sum[0][1] += a[i] * a[i];
+		}
+		for (int i = 1; i <= n; ++i) {
+			scanf("%d", b + i);
+			b[n + i] = b[i];
+			sum[1][0] += b[i];
+			sum[1][1] += b[i] * b[i];
+		}
+		int len = 1;
+		while (len < n + m) {
+			len <<= 1;
+		}
+		for (int i = 0; i < len; ++i) {
+			x1[i] = Complex(0, 0);
+			x2[i] = Complex(0, 0);
+		}
+		for (int i = 0; i < n; ++i) {
+			x1[i] = Complex(a[i + 1], 0);
+		}
+		for (int i = 0; i < m; ++i) {
+			x2[i] = Complex(b[i + 1], 0);
+		}	
+		fft(x1, len, 1);
+		fft(x2, len, 1);
+		for (int i = 0; i < len; ++i) {
+			x1[i] = x1[i] * x2[i];	
+		}
+		fft(x1, len, -1);
+		for (int i = 0; i < len; ++i) {
+			c[i] = (ll)(x1[i].x + 0.5);
+		}
+		ll Max = -INF;
+		for (int i = n; i < 2 * n; ++i) {
+			Max = max(Max, c[i]);
+		}
+		ll res = INF;
+		for (int i = -200; i <= 200; ++i) {
+			res = min(res, sum[0][1] + sum[1][1] + 1ll * n * i * i + 2ll * i * (sum[0][0] - sum[1][0]) - 2ll * Max);
+		}
+		printf("%lld\n", res);
+	}
+	return 0;
+}
---
+++ atcoder/AGC033/a.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 1010
+int G[N][N];
+char s[N];
+int n, m;
+int cnt;
+int Move[][2] = {
+	-1, 0,
+	 1, 0,
+	 0,-1,
+	 0, 1,
+};
+bool ok(int x, int y) {
+	if (x < 1 || x > n || y < 1 || y > m) {
+		return false;
+	}
+	return true;
+}
+
+int main() {
+	while (scanf("%d%d", &n, &m) != EOF) {
+		cnt = 0;
+		for (int i = 1; i <= n; ++i) {
+			scanf("%s", s + 1); 
+			for (int j = 1; j <= m; ++j) {
+				if (s[j] == '#') {
+					++cnt;
+					G[i][j] = 0;
+				} else {
+					G[i][j] = n * m; 
+				}
+			}
+		}
+		int res = 0;
+		while (cnt < n * m) {
+			++res;
+			for (int i = 1; i <= n; ++i) {
+				for (int j = 1; j <= m; ++j) if (G[i][j] == n * m) {
+					for (int k = 0; k < 4; ++k) {
+						int nx = i + Move[k][0];
+						int ny = j + Move[k][1];
+						if (ok(nx, ny) && G[nx][ny] < res) {
+							G[i][j] = res;
+							++cnt;
+							break;
+						}
+					}
+				}
+			}
+		}
+		printf("%d\n", res);
+	}
+	return 0;
+}
