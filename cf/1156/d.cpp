c28450494be247f3e577a684f1dd3f30357e6f1d
[Dup4] <[panlvzhi@foxmail.com]>
2019-05-04 11:51:58 +0800

    up

--- cf/1156/d.cpp
+++ cf/1156/d.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 #define ll long long
 #define N 200010
-int n;
+int n, vis[N];
+ll res;
 struct Graph {
 	struct node {
 		int to, nx, w;
@@ -27,7 +28,7 @@ struct Graph {
 int root, sum, f[N], sze[N];
 void getroot(int u, int fa) {
 	sze[u] = 1; f[u] = 0;
-	erp(u) if (v != fa) {
+	erp(u) if (v != fa && !vis[v]) {
 		getroot(v, u);
 		sze[u] += sze[v];
 		f[u] = max(f[u], sze[v]);
@@ -38,21 +39,126 @@ void getroot(int u, int fa) {
 	}
 }
 
-ll calc(int u) {
-		
+//0代表全0 1代表全1 2代表上0下1 3代表上1下0 4代表根节点 5代表混合
+int deep[N], cnt[10]; 
+void getdeep(int u, int fa) {
+	switch(deep[u]) {
+		case 0 :
+			res += 1 + cnt[0]; 
+			res += 1 + cnt[0] + cnt[1] + cnt[2]; 
+			break;
+		case 1 :
+			res += 1 + cnt[0] + cnt[1] + cnt[3];
+			res += 1 + cnt[1]; 
+			break;
+		case 2 :
+			res += 1 + cnt[0];
+			break;
+		case 3 :
+			res += 1 + cnt[1];
+			break;
+		case 4 :
+		case 5 :
+			break;
+	}
+	erp(u) if (v != fa && !vis[v]) {
+		switch (w) {
+			case 0 :
+				switch (deep[u]) {
+					case 0 :
+						deep[v] = 0;
+						break;
+					case 1 :
+						deep[v] = 3;
+						break;
+					case 2 :
+						deep[v] = 5;
+						break;
+					case 3 :
+						deep[v] = 3;
+						break;
+					case 4 :
+						deep[v] = 0;
+						break;
+					case 5 :
+						deep[v] = 5;
+						break;
+					default :
+						assert(0);
+				}
+				break;
+			case 1 :
+				switch (deep[u]) {
+					case 0 :
+						deep[v] = 2;
+						break;
+					case 1 :
+						deep[v] = 1;
+						break;
+					case 2 :
+						deep[v] = 2;
+						break;
+					case 3 :
+						deep[v] = 5;
+						break;
+					case 4 :
+						deep[v] = 1;
+						break;
+					case 5 :
+						deep[v] = 5;
+						break;
+					default :
+						assert(0);
+				}
+				break;
+			default :
+				assert(0);
+		}
+		getdeep(v, u);
+	}
+}
+
+void add(int u, int fa) {
+	++cnt[deep[u]];
+	erp(u) if (v != fa && !vis[v]) {
+		add(v, u);
+	}
+}
+
+void solve(int u) {
+	vis[u] = 1;
+	memset(cnt, 0, sizeof cnt);
+	deep[u] = 4;
+	++cnt[4];   
+	erp(u) if (!vis[v]) {
+		deep[v] = w;
+		getdeep(v, u);
+		add(v, u);  
+	}
+	erp(u) if (!vis[v]) {
+		sum = f[0] = sze[v];
+		root = 0;
+		getroot(v, 0);
+		solve(root);
+	}
 }
 
 void init() {
 	G.init(n);
+	res = 0;
+	memset(vis, 0, sizeof vis); 
 }
 int main() {
 	while (scanf("%d", &n) != EOF) {
 		init();
 		for (int i = 1, u, v, w; i < n; ++i) {
 			scanf("%d%d%d", &u, &v, &w);
-			G.add(u, v, w);
+			G.add(u, v, w); 
 		}
-		
+		sum = f[0] = n; root = 0;
+		getroot(1, 0);
+		solve(root);
+		printf("%lld\n", res);
 	}
 	return 0;
 }
