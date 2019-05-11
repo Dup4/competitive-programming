c18ee3605d35b61629078b24cf45aa7a7860fc9d
[Dup4] <[panlvzhi@foxmail.com]>
2019-05-11 08:49:15 +0800

    up

--- tem/a.cpp
+++ tem/a.cpp
@@ -1,58 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int G[11][11];
-int f[11][11][11][11];
-int Move[][2] = {
-	{1, 0},
-	{0, 1},
-};
-
+#define N 11
+int n, res[N];
+int used[N][N];
 bool ok(int x, int y) {
-	if (x < 1 || x > n || y < 1 || y > n) {
-		return false;
-	}
-	return true;
-}
-int dp(int x, int y, int z, int w) {
-	if (f[x][y][z][w] != -1) {
-		return f[x][y][z][w];
-	} 
-	f[x][y][z][w] = 0;
-	for (int i = 0; i < 2; ++i) {
-		int nx = x + Move[i][0];
-		int ny = y + Move[i][1];
-		for (int j = 0; j < 2; ++j) {
-			int nz = z + Move[j][0];
-			int nw = w + Move[j][1];
-			if (ok(nx, ny) && ok(nz, nw)) {
-				f[x][y][z][w] = max(f[x][y][z][w], dp(nx, ny, nz, nw));
+	for (int i = 1; i < x; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			if (i + j == x + y || i - j == x - y || j == y) {
+				if (used[i][j]) {
+					return false;
+				}
 			}
 		}
 	}
-	if (x == z && y == w) {
-		f[x][y][z][w] += G[x][y];
-	} else {
-		f[x][y][z][w] += G[x][y] + G[z][w];
+	return true;	
+}
+void DFS(int cur) {
+	if (cur == n + 1) {
+		++res[n];
+		return;
 	}
-	return f[x][y][z][w];
+	for (int i = 1; i <= n; ++i) {
+		if (ok(cur, i)) {
+			used[cur][i] = 1;
+			DFS(cur + 1); 
+			used[cur][i] = 0;
+		}
+	}
 }
 
 int main() {
-	while (scanf("%d", &n) != EOF) {
-		memset(G, 0, sizeof G);
-		memset(f, -1, sizeof f);
-		int x, y, d;
-		while (scanf("%d%d%d", &x, &y, &d), x || y || d) {
-			G[x][y] = d;
+	memset(res, -1, sizeof res);
+	while (scanf("%d", &n), n) {
+		if (res[n] != -1) {
+			printf("%d\n", res[n]);
+		} else {
+			memset(used, 0, sizeof used);
+			res[n] = 0;
+			DFS(1);
+			printf("%d\n", res[n]); 
 		}
-	//	for (int i = 1; i <= n; ++i) {
-	//		for (int j = 1; j <= n; ++j) {
-	//			printf("%d%c", G[i][j], " \n"[j == n]);
-	//		}
-	//	}
-		printf("%d\n", dp(1, 1, 1, 1));
 	}
 	return 0;
 }
