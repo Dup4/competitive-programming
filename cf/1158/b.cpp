8f3c39e9f26896fb5bffb0b991b44076354d5a0e
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-05-13 06:39:08 +0800

    up

---
+++ cf/1158/b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 100010
+int n, k;
+
+int main() {
+	while (scanf("%d%d", &n, &k) != EOF) {
+		int remind = (n - k) / 2;
+		for (int i = 
+	}
+	return 0;
+}
