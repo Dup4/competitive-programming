57bcc06ec0996804fa1fa3eebacc9dfbc942d1a9
[Dup4] <[panlvzhi@foxmail.com]>
2019-05-16 18:39:21 +0800

    up

--- tem/c++hw/b.cpp
+++ tem/c++hw/b.cpp
@@ -1,21 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+#define ll unsigned long long
 int main() {
-	int a[3];
-	ifstream in("image.jpg", ios::binary);
+	ll a[3];
+	ifstream in;
+	in.open("image.jpg", ios::binary);
 	in.seekg(12L, ios::beg);
-	in.read(reinterpret_cast< char * > (&a[0]), 1);
-	//cout << in.tellg() << endl;
+	in.read(reinterpret_cast< char * > (&a[0]), 1);   
 	in.seekg(48L, ios::beg);
 	in.read(reinterpret_cast< char * >(&a[1]), 1); 
-	//cout << in.tellg() << endl;
 	in.seekg(79L, ios::beg);
 	in.read(reinterpret_cast<char *>(&a[2]), 1);
-	//cout << in.tellg() << endl;
-	//cout << a[0] << " " << a[1] << " " << a[2] << endl;
-	cout << "a9\n";
-	//cout << hex << (a[0] ^ a[1] ^ a[2]) << endl;
+	int res = 192;
+//	string ss = "";
+//	while (res) {
+//		int x = res % 16;
+//		res /= 16;
+//		if (x  < 10) {
+//			ss += x + '0';
+//		} else if (x == 10) {
+//			ss += 'a';
+//		} else if (x == 11) {
+//			ss += 'b';
+//		} else if (x == 12) {
+//			ss += 'c';
+//		} else if (x == 13) {
+//			ss += 'd';
+//		} else if (x == 14) {
+//			ss += 'e';
+//		} else if (x == 15) {
+//			ss += 'f';
+//		}
+//	}
+//	reverse(ss.begin(), ss.end());
+//	cout << ss << endl;
+	cout << hex << res << endl;	
 	return 0;
 }
