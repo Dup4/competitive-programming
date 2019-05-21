c149098cce17f3a97824ed6ce6966e03b421f472
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-05-21 08:46:39 +0800

    up

--- hdu/6540/a.cpp
+++ hdu/6540/a.cpp
@@ -1,21 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
 #define N 5010
+const int p = 1e9 + 7;
 int n, m, k;
 vector < vector <int> > G;
-int vis[N], f[N][N];
+int vis[N], f[N][N], g[N];
+void add(int &x, int y) {
+	x += y;
+	if (x >= p) {
+		x -= p;
+	}
+}
+void sub(int &x, int y) {
+	x -= y;
+	if (x < 0) {
+		x += p;
+	}
+}
 
 void DFS(int u, int fa) {
 	for (int i = 0; i <= k; ++i) {
-		fa[u][i] = 0;
+		f[u][i] = 0;
 	}
 	if (vis[u]) {
 		f[u][0] = 1;
 	}
 	for (auto v : G[u]) if (v != fa) {
 		DFS(v, u);
-		
+		for (int i = 0; i <= k; ++i) {
+			g[i] = f[u][i];
+			if (i) {
+				add(g[i], f[v][i - 1]);
+			}
+		}
+		for (int i = 1; i <= k; ++i) {
+			add(f[v][i], f[v][i - 1]);
+		}
+		for (int i = 1; i <= k; ++i) {
+			add(g[i], 1ll * f[u][i] * f[v][min(k - i, i - 1)] % p);
+		}
+		for (int i = k; i >= 1; --i) {
+			sub(f[v][i], f[v][i - 1]);
+		}
+		for (int i = 1; i <= k; ++i) {
+			add(f[u][i], f[u][i - 1]);
+		}
+		for (int i = 0; i <= k; ++i) {
+			add(g[i + 1], 1ll * f[v][i] * f[u][min(k - i, i)] % p);
+		}
+		for (int i = 0; i <= k; ++i) {
+			f[u][i] = g[i];
+		}
 	}
 }
 
@@ -36,7 +73,12 @@ int main() {
 			scanf("%d", &x);
 			vis[x] = 1;
 		}
-		
+		DFS(1, 1);
+		int res = 0;
+		for (int i = 0; i <= k; ++i) {
+			add(res, f[1][i]);
+		}	
+		printf("%d\n", res);
 	}
 	return 0;
 }
