3d7cfc5be2704b6f2f452921b74e8042759b9bef
[Dup4] <[panlvzhi@foxmail.com]>
2019-05-27 07:45:17 +0800

    up

---
+++ cf/1169/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 110
+int n, a, x, b, y;
+
+int main() {
+	while (scanf("%d%d%d%d%d", &n, &a, &x, &b, &y) != EOF) {
+		bool F = (a == b);
+		while (a != x && b != y) {
+			++a;
+			if (a > n) a = 1;
+			--b;
+			if (b <= 0) b = n;
+			F |= (a == b);	
+		}
+		puts(F ? "YES" : "NO");
+	}
+	return 0;
+}
