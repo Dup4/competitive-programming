7d082e128326da1542cd0c91705a195566498d04
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-05-28 10:56:51 +0800

    up

--- jisuanke/2625/b.cpp
+++ jisuanke/2625/b.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 2000010
+#define N 3000010
 #define M 100010
 #define ll long long
 ll n, m, p;
 int prime[N];
 bool check[N], vis[M];
-ll f[N], F[N], phi[N]; 
+ll f[N], g[N], F[N];      
 template <class T>
 void add(T &x, T y) {
 	x += y;
@@ -21,17 +21,20 @@ void sub(T &x, T y) {
 	if (x < 0) {
 		x += p;
 	}
-}
+} 
 
 void init() {
 	memset(f, 0, sizeof f);
 	memset(check, 0, sizeof check);
+	memset(vis, 0, sizeof vis);
 	prime[0] = 0;
-	phi[1] = 1;
+	f[1] = 1;
+	g[1] = 1;
 	for (int i = 2; i < N; ++i) {
 		if (!check[i]) {
 			prime[++prime[0]] = i;
-			phi[i] = i - 1;
+			g[i] = i;
+			f[i] = 2 * i - 1;
 		}
 		for (int j = 1; j <= prime[0]; ++j) {
 			if (1ll * i * prime[j] >= N) {
@@ -39,48 +42,43 @@ void init() {
 			}
 			check[i * prime[j]] = 1;
 			if (i % prime[j] == 0) {
-				phi[i * prime[j]] = 1ll * phi[i] * prime[j];
+				g[i * prime[j]] = g[i] * prime[j];
+				f[i * prime[j]] = f[i / g[i]] * (f[g[i]] * prime[j] % p + 1ll * (prime[j] - 1) * g[i] % p) % p;   
 				break;
 			} else {
-				phi[i * prime[j]] = 1ll * phi[i] * (prime[j] - 1);
+				g[i * prime[j]] = prime[j]; 
+				f[i * prime[j]] = f[i] * f[prime[j]] % p;
 			}
 		}
 	}
-	for (int i = 1; i < N; ++i) {
-		for (int j = i; j < N; j += i) {
-			add(f[j], 1ll * i * phi[j / i] % p);
-		}
-	}
 	for (int i = 2; i < N; ++i) {
-		add(f[i], f[i - 1]);
+		add(f[i], f[i - 1]);  
 	}
+}
+
+ll sumn(int n) {
+	return (1ll * n * (n + 1) / 2) % p;
+}
+
+ll get_d(int n) {
+	ll res = 0;
+	for (int i = 1, j; i <= n; i = j + 1) {
+		j = n / (n / i);
+		add(res, 1ll * (sumn(j) - sumn(i - 1) + p) % p * sumn(n / i));
+	}   
+	return res;
 }
 
 ll get_f(int x) {
-	if (x < N) {
-		return f[x];
-	} else {
-		return F[n / x];
-	}
-}
-
-ll get_one(int n) {
-	return (1ll * n * (n - 1) / 2) % p;
-}
-ll get_two(int n) {
-	return (1ll * (n - 1) * (n - 1) % p + 2ll * (n - 1) % p + 1) % p;
-}
-
-void work(int x) {
-	if (x < N) return;
+	if (x < N) return f[x];
 	int t = n / x;
-	if (vis[t]) return; vis[t] = 1; 
-	F[t] = get_two(x);  
+	if (vis[t]) return F[t]; vis[t] = 1; 
+	F[t] = get_d(x);  
 	for (int i = 2, j; i <= x; i = j + 1) {
 		j = x / (x / i);
-		work(x / i);
-		sub(F[t], (get_one(j) - get_one(i - 1) + p) % p * get_f(x / i) % p); 
+		sub(F[t], 1ll * (j - i + 1) % p * get_f(x / i) % p); 
 	}
+	return F[t];
 }
 
 ll qmod(ll base, ll n) {
@@ -98,13 +96,11 @@ ll qmod(ll base, ll n) {
 
 int main() {
 	while (scanf("%lld%lld%lld", &n, &m, &p) != EOF) {
-		memset(vis, 0, sizeof vis);
-		init(); 
 		--p;
+		init(); 
 		ll res = 0;
 		for (int i = 1, j; i <= n; i = j + 1) { 
 			j = n / (n / i);
-			work(j); work(i - 1);
 			add(res, 1ll * (n / i) * (n / i) % p * (get_f(j) - get_f(i - 1) + p) % p); 
 		}
 		++p; 
