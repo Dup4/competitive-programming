06375bf631cf7b33ff4cace80bbd801a66e06183
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-06-06 08:37:24 +0800

    up

--- cf/1175/dd.cpp
+++ cf/1175/dd.cpp
@@ -1,42 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+#define ll long long 
 #define N 300010
 int n, k;
-ll a[N], sum[N]; 
+ll a[N];
+
 
 int main() {
 	while (scanf("%d%d", &n, &k) != EOF) {
-		sum[0] = 0;
-		for (int i = 1; i <= n; ++i) {
+		for (int i = 1; i <= n; ++i) {  
 			scanf("%lld", a + i);
-			sum[i] = sum[i - 1] + a[i];
 		}
-		if (k == 1) {
-			printf("%lld\n", sum[n]);
-		} else if (k == 2) {
-			ll res = -1e18;
-			for (int i = 1; i < n; ++i) {
-				res = max(res, sum[i] + 2ll * (sum[n] - sum[i]));
-			}
-			printf("%lld\n", res);
-		} else {
-			ll res = -1e18;
-			ll base = 0;
-			for (int i = 2; i < k; ++i) {
-				base += 1ll * i * a[i]; 
-			}
-			for (int i = 1; i + k - 2 < n; ++i) {
-				ll tmp = sum[i] + base + 1ll * k * (sum[n] - sum[i + k - 2]);
-				res = max(res, tmp);
-				base -= 2ll * a[i + 1];
-				base += 1ll * (k - 1) * a[i + k - 1]; 
-				base -= sum[i + k - 2] - sum[i + 1]; 
-			}
-			printf("%lld\n", res);
+		for (int i = n - 1; i >= 0; --i) {
+			a[i] += a[i + 1];
 		}
+		sort(a + 2, a + n + 1, [&](int x, int y) {
+			return x > y;		
+		});
+		ll ans = 0;
+		for (int i = 1; i <= k; ++i) {
+			ans += a[i];
+		}
+		printf("%lld\n", ans);
 	}
 	return 0;
 }
-
