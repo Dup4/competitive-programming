acd2d540e111189962312d42c4f97029fd790f28
[Dup4] <[panlvzhi@foxmail.com]>
2019-06-11 12:40:16 +0800

    up

--- cf/1176/ff.cpp
+++ cf/1176/ff.cpp
@@ -3,26 +3,28 @@ using namespace std;
 
 #define ll long long
 #define N 200010
+#define pii pair <int, int>
+#define fi first
+#define se second
 int n, m;
-ll f[N][10];
-
+ll f[N][10]; 
 
 int main() {
 	while (scanf("%d", &n) != EOF) {
 		memset(f, 0, sizeof f);
 		for (int i = 1; i <= n; ++i) {
-			vector <int> vec[4], tmp;
+			vector <pii> vec[4], tmp;  
 			scanf("%d", &m);
 			for (int j = 1, c, d; j <= m; ++j) {
 				scanf("%d%d", &c, &d);
-				vec[c].push_back(d);
+				vec[c].push_back(pii(d, j)); 
 			}
 			for (int j = 1; j <= 3; ++j) {
 				sort(vec[j].begin(), vec[j].end());
 			}
 			// 一张
 			if (vec[3].size() >= 1) {
-				ll Max = vec[3].back();
+				ll Max = vec[3].back().fi;
 				for (int j = 1; j < 9; ++j) {
 					f[i][j] = max(f[i][j], f[i - 1][j - 1] + Max); 
 				}
@@ -30,35 +32,46 @@ int main() {
 			}
 			// 二张
 			if (vec[1].size() >= 1 && vec[2].size() >= 1) {
-				tmp.clear();
-				tmp.push_back(vec[1].back());
+				tmp.clear(); 
+				tmp.push_back(vec[1].back()); 
 				tmp.push_back(vec[2].back());
-				sort(tmp.begin(), tmp.end(), [&](int x, int y) {
-					return x > y;
+				sort(tmp.begin(), tmp.end(), [&](pii x, pii y) {
+					return x.fi > y.fi; 
 				});
-				ll tot = tmp[0] + tmp[1];
+				ll tot = tmp[0].fi + tmp[1].fi;
 				for (int j = 0; j < 8; ++j) {
 					f[i][j + 2] = max(f[i][j + 2], f[i - 1][j] + tot);
 				}
-				f[i][0] = max(f[i][0], f[i - 1][8] + tot + tmp[0]); 
-				f[i][1] = max(f[i][1], f[i - 1][9] + tot + tmp[0]);
+				if (tmp[0].se > tmp[1].se) {
+					swap(tmp[0], tmp[1]);
+				}
+				f[i][0] = max(f[i][0], f[i - 1][8] + tot + tmp[1].fi); 
+				f[i][1] = max(f[i][1], f[i - 1][9] + tot + tmp[0].fi);
 			}
 			//三张
-			if (vec[1].size() >= 3) {
+			if (vec[1].size() >= 3) { 
 				tmp.clear();
 				for (int j = 1; j <= 3; ++j) {
 					tmp.push_back(vec[1].end()[-j]);
 				}
-				sort(tmp.begin(), tmp.end(), [&](int x, int y) {
-					return x > y;
+				sort(tmp.begin(), tmp.end(), [&](pii x, pii y) {
+					return x.se < y.se;
 				});
-				ll tot = accumulate(tmp.begin(), tmp.end(), 1ll);
+				ll tot = 0;
+				for (int j = 0; j < 3; ++j) {
+					tot += tmp[j].fi;
+				}
 				for (int j = 0; j < 7; ++j) {
 					f[i][j + 3] = max(f[i][j + 3], f[i - 1][j] + tot);
 				}
-				f[i][0] = max(f[i][0], f[i - 1][7] + tot + tmp[0]); 
-				f[i][1] = max(f[i][1], f[i - 1][8] + tot + tmp[0]);
-				f[i][2] = max(f[i][2], f[i - 1][9] + tot + tmp[0]);
+				f[i][0] = max(f[i][0], f[i - 1][7] + tot + tmp[2].fi); 
+				f[i][1] = max(f[i][1], f[i - 1][8] + tot + tmp[1].fi);
+				f[i][2] = max(f[i][2], f[i - 1][9] + tot + tmp[0].fi);
+			}
+		}
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 0; j < 10; ++j) {
+				printf("%d %d %lld\n", i, j, f[i][j]);
 			}
 		}
 		ll res = 0;
--- cf/1176/fff.cpp
+++ cf/1176/fff.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 #define ll long long
 #define N 200010
-#define pii pair <int, int>
-#define fi first
-#define se second
 int n, m;
 ll f[N][10];
 
@@ -13,12 +10,13 @@ ll f[N][10];
 int main() {
 	while (scanf("%d", &n) != EOF) {
 		memset(f, 0, sizeof f);
+		f[0][0] = 0;
 		for (int i = 1; i <= n; ++i) {
-			vector <pii> vec[4], tmp;
+			vector <int> vec[4], tmp;
 			scanf("%d", &m);
 			for (int j = 1, c, d; j <= m; ++j) {
 				scanf("%d%d", &c, &d);
-				vec[c].push_back(pii(d, j));
+				vec[c].push_back(d);
 			}
 			for (int j = 1; j <= 3; ++j) {
 				sort(vec[j].begin(), vec[j].end());
@@ -28,9 +26,9 @@ int main() {
 				f[i][j] = f[i - 1][j];
 			}
 			// 一张
-			ll Max = 0;
+			int Max = 0;
 			for (int j = 1; j <= 3; ++j) if (!vec[j].empty()) {
-				Max = max(Max, 1ll * vec[j].back().fi);
+				Max = max(Max, vec[j].back());
 			}
 			for (int j = 0; j < 9; ++j) {
 				f[i][j + 1] = max(f[i][j + 1], f[i - 1][j] + Max); 
@@ -49,39 +47,36 @@ int main() {
 				tmp.push_back(vec[2].end()[-1]);
 			}
 			if (tmp.size() >= 2) {
-				sort(tmp.begin(), tmp.end(), [&](pii x, pii y) {
-					return x.fi > y.fi;
+				sort(tmp.begin(), tmp.end(), [&](int x, int y) {
+					return x > y;
 				});
-				ll tot = tmp[0].fi + tmp[1].fi;
+				ll tot = tmp[0] + tmp[1];
 				for (int j = 0; j < 8; ++j) {
 					f[i][j + 2] = max(f[i][j + 2], f[i - 1][j] + tot);
 				}
-				if (tmp[0].se < tmp[1].se) {
-					swap(tmp[0], tmp[1]);
-				}
-				f[i][0] = max(f[i][0], f[i - 1][8] + tot + tmp[0].fi); 
-				f[i][1] = max(f[i][1], f[i - 1][9] + tot + tmp[1].fi);
+				f[i][0] = max(f[i][0], f[i - 1][8] + tot + tmp[0]); 
+				f[i][1] = max(f[i][1], f[i - 1][9] + tot + tmp[0]);
 			}
 			
 			//三张
 			if (vec[1].size() >= 3) {
 				tmp.clear();
 				for (int j = 1; j <= 3; ++j) {
-					tmp.push_back(vec[1].end()[-j]);  
+					tmp.push_back(vec[1].end()[-j]);
 				}
-				sort(tmp.begin(), tmp.end(), [&](pii x, pii y) {
-					return x.se > y.se;
+				sort(tmp.begin(), tmp.end(), [&](int x, int y) {
+					return x > y;
 				});
 				ll tot = 0;
 				for (int j = 0; j < 3; ++j) {
-					tot += tmp[j].fi;
+					tot += tmp[j];
 				}
 				for (int j = 0; j < 7; ++j) {
 					f[i][j + 3] = max(f[i][j + 3], f[i - 1][j] + tot);
 				}
-				f[i][0] = max(f[i][0], f[i - 1][7] + tot + tmp[0].fi); 
-				f[i][1] = max(f[i][1], f[i - 1][8] + tot + tmp[1].fi);
-				f[i][2] = max(f[i][2], f[i - 1][9] + tot + tmp[2].fi);
+				f[i][0] = max(f[i][0], f[i - 1][7] + tot + tmp[0]); 
+				f[i][1] = max(f[i][1], f[i - 1][8] + tot + tmp[0]);
+				f[i][2] = max(f[i][2], f[i - 1][9] + tot + tmp[0]);
 			}
 		}
 		ll res = 0;
