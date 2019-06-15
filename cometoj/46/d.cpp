c37f1ec82f0e15a138ee80efe7d6d49c05e3cbf7
[Dup4] <[panlvzhi@foxmail.com]>
2019-06-15 14:37:43 +0800

    up

--- cometoj/46/d.cpp
+++ cometoj/46/d.cpp
@@ -3,18 +3,16 @@ using namespace std;
 
 #define ll long long
 #define N 150010
-#define M 131072
 #define S 400
 #define pii pair <int, int>
 #define fi first
 #define se second
-const int p = 998244353;
+const ll p = 998244353;
 int n, m, q, b[N], d[N], vis[N], id[N], fid[N], cnt_id;
 set <pii> G[N]; vector <pii> vec; 
 int e[N][3];  
 
-template <typename T>
-void add(T &x, T y) {
+void add(ll &x, ll y) {
 	x += y;
 	if (x >= p) {
 		x -= p;
@@ -24,9 +22,9 @@ void add(T &x, T y) {
 struct TRIE {
 	struct node {
 		int son[2];
-		int tot;
+		ll tot;
 		node() {
-			memset(son, -1, sizeof son);
+			memset(son, -1, sizeof son); 
 			tot = 0;
 		}
 	}t[N * 200]; 
@@ -34,14 +32,14 @@ struct TRIE {
 	void init() {
 		for (int i = 1; i <= S; ++i) { 
 			rt[i] = i;
-			t[i] = node();
+			t[i] = node();  
 		}
 		cnt = S; 
-		t[0] = node();
+		t[0] = node(); 
 	}
 	void insert(int id, int x, int y) {
-		int root = rt[id];
-		for (int i = 17; i >= 0; --i) {
+		int root = rt[id]; 
+		for (int i = 17; i >= 0; --i) {  
 			int nx = t[root].son[(x >> i) & 1];  
 			if (nx == -1) {
 				nx = ++cnt;
@@ -51,40 +49,42 @@ struct TRIE {
 			root = nx;
 		}
 	}
-	int query(int id, int x, int y) {
-		int res = 0;
+	ll query(int id, int x, int y) {
+		ll res = 0;
 		int root = rt[id];
 		for (int i = 17; i >= 0; --i) {
-			int f = 0 ^ ((x >> i) & 1); 
-			if ((y >> i) & 1) {  
+			int f = ((x >> i) & 1); 
+			int g = ((y >> i) & 1); 
+			if (g) {  
 				add(res, t[t[root].son[f]].tot);	
 			} 
-			root = t[root].son[((y >> i) & 1) ^ f];       
+			root = t[root].son[g];        
 		}
+		add(res, t[root].tot);
 		return res; 
 	}
 }trie;
 
 struct BIT {
-	int a[N];
+	ll a[N];
 	void init() {
 		memset(a, 0, sizeof a);
 	}
-	void update(int x, int v) {
+	void update(int x, ll v) {
 		++x; 
 		for (; x < N; x += x & -x) {
 			add(a[x], v);  
 		}
 	}
-	int query(int x) {
+	ll query(int x) {
 		++x;
-		int res = 0;
+		ll res = 0;
 		for (; x > 0; x -= x & -x) {
 			add(res, a[x]); 
 		}
 		return res;
 	}
-	int query(int l, int r) {
+	ll query(int l, int r) {
 		return (query(r) - query(l - 1) + p) % p;
 	}
 }bit;
@@ -108,7 +108,6 @@ int main() {
 			int &x = e[i][0], &y = e[i][1], &z = e[i][2];  
 			scanf("%d%d%d", &x, &y, &z); 
 			++d[x]; ++d[y]; 
-			add(z, 0);
 			G[x].insert(pii(y, z));
 			G[y].insert(pii(x, z));
 		}
@@ -123,7 +122,7 @@ int main() {
 			vec.clear();
 			for (auto it : G[i]) {
 				if (!vis[it.fi]) {
-					trie.insert(trie.rt[id[i]], b[it.fi], it.se); 
+					trie.insert(id[i], b[it.fi], it.se); 
 				} else {
 					vec.push_back(it); 
 				}
@@ -145,7 +144,8 @@ int main() {
 				swap(e[i][0], e[i][1]);
 			}
 		}
-		int op, x, y, res;
+		int op, x, y, u, v, w;
+		ll res;
 		while (q--) {
 			scanf("%d%d%d", &op, &x, &y);
 			switch(op) {
@@ -156,9 +156,10 @@ int main() {
 						bit.update(y ^ b[it.fi], it.se);
 					}
 					if (!vis[x]) {
-						for (auto it : G[x]) {
+						for (auto it : G[x]) {  
 							if (vis[it.fi]) {
-								trie.insert(trie.rt[id[it.fi]], b[x], (y -it.se + p) % p);
+								trie.insert(id[it.fi], b[x], (p - it.se) % p);
+								trie.insert(id[it.fi], y, it.se);
 							}
 						}		
 					}
@@ -166,26 +167,25 @@ int main() {
 					break;
 				//修改边权
 				case 2 :
-					add(y, 0);
-					if (vis[e[x][0]] == vis[e[x][1]]) {
-						bit.update(b[e[x][0]] ^ b[e[x][1]], (y - e[x][2] + p) % p);
-						G[e[x][0]].erase(pii(e[x][1], e[x][2]));
-						G[e[x][1]].erase(pii(e[x][0], e[x][2]));
-						e[x][2] = y;
-						G[e[x][0]].insert(pii(e[x][1], e[x][2]));
-						G[e[x][1]].insert(pii(e[x][0], e[x][2]));
+					u = e[x][0], v = e[x][1], w = e[x][2];
+					if (vis[u] == vis[v]) {
+						bit.update(b[u] ^ b[v], (y - w + 2ll * p) % p); 
+						G[u].erase(pii(v, w));
+						G[v].erase(pii(u, w));
+						G[u].insert(pii(v, y));
+						G[v].insert(pii(u, y));  
 					} else {
-						trie.insert(trie.rt[id[e[x][0]]], b[e[x][1]], (y - e[x][2] + p) % p);
-						e[x][2] = y;
+						trie.insert(id[u], b[v], (y - w + 2ll * p) % p);
 					}
+					e[x][2] = y;
 					break;
 				//询问
 				case 3 :
 					res = bit.query(x, y); 
-					for (int i = 1; i <= cnt_id; ++i) {
-						add(res, (trie.query(trie.rt[i], b[fid[i]], y + 1) - trie.query(trie.rt[i], b[fid[i]], x) + p) % p);  
+					for (int i = 1; i <= cnt_id; ++i) {  
+						add(res, (trie.query(i, b[fid[i]], y) - trie.query(i, b[fid[i]], x - 1) + p) % p);  
 					}
-					printf("%d\n", res);
+					printf("%lld\n", res);
 					break;
 				default: 
 					assert(0);
--- nowcoder/921/a.cpp
+++ nowcoder/921/a.cpp
@@ -1,14 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class A {
-public :
-	int a;
-	A(int a) {
-		this->a = a;
-	}
-};
-
 int main() {
+
 	return 0;
 }
