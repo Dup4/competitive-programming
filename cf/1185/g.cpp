cbf300a80b8bd66c4acc8b97f557d25b076637cb
[Dup4] <[panlvzhi@foxmail.com]>
2019-06-20 12:20:08 +0800

    up

--- cf/1185/g.cpp
+++ cf/1185/g.cpp
@@ -6,51 +6,43 @@ using namespace std;
 const ll p = 1e9 + 7;  
 int n, T;
 int t[N], g[N];
-ll fac[N], inv[N];
-ll qmod(ll base, ll n) {
-	ll res = 1;
-	while (n) {
-		if (n & 1) {
-			res = res * base % p;
-		}
-		base = base * base % p;
-		n >>= 1;
-	}
-	return res;
-}
-ll C(int n, int m) {
-	if (n < m) {
-		return 0;
-	}
-	return fac[n] * inv[n - m] % p * inv[m] % p;
-}
-ll f(int x, int y, int z) {
-	if (x == 0 && y == 0) {
-		if (z == 1) {
-			return 1;
-		}
-	} else if (x == 0) {  
-		if (y == z - 1) {
-			return fac[z] * fac[y] % p;
-		} 
-	} else {
-		if (x + y < z - 1) {
-			return 0;
-		} else {
-			return fac[z] * C(z - 1, y) % p * C(x, z - 1 - y)
-		}
-	}
-	return 0;
-}	
+ll f[20][20][20];
 	
-int main() {
-	fac[0] = 1;
-	for (int i = 1; i <= 20; ++i) {
-		fac[i] = fac[i - 1] * i % p;
+void add(ll &x, ll y) {
+	x += y;
+	if (x >= p) {
+		x -= p;
 	}
-	inv[20] = qmod(fac[20], p - 2);
-	for (int i = 20; i >= 1; --i) {
-		inv[i - 1] = inv[i] * i % p;
+}
+
+int main() {
+	memset(f, 0, sizeof f);
+	f[1][0][0] = 1;
+	f[0][1][0] = 1;
+	f[0][0][1] = 1;
+	for (int i = 0; i <= 15; ++i) {
+		for (int j = 0; j <= 15; ++j) {
+			for (int k = 0; k <= 15; ++k) {
+				if (i + j + k >= 2) {
+					int tot = i + j + k;
+					if (i > 0 && j > 0 && k > 0) {
+					
+					} else if (i > 0 && j > 0) {
+					
+					} else if (i > 0 && k > 0) {
+						
+					} else if (j > 0 && k > 0) {
+					
+					} else if (i > 0) {
+						add(f[i][j][k], f[i - 1])
+					} else if (j > 0) {
+						
+					} else if (k > 0) {
+						
+					}
+				}
+			}
+		}
 	}
 	while (scanf("%d%d", &n, &T) != EOF) {
 		for (int i = 1; i <= n; ++i) {
@@ -67,8 +59,7 @@ int main() {
 				}
 			}
 			if (tot == T) {
-				sort(x + 1, x + 4);
-				(res += f(x[1], x[2], x[3])) %= p; 
+				add(res, f[x[1]][x[2]][x[3]]);
 			}
 		}
 		printf("%lld\n", res); 
