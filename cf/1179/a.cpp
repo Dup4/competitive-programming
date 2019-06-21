a2201b52febf349345d64a24b5cad99b36af1479
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-06-22 07:15:13 +0800

    up

---
+++ cf/1179/a.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 400010
+#define pii pair <int, int>
+#define fi first
+#define se second
+int n, q, a[N], b[N]; 
+pii res[N]; 
+
+int main() {
+	while (scanf("%d%d", &n, &q) != EOF) {
+		int Max = 0;
+		for (int i = 1; i <= n; ++i) {
+			scanf("%d", a + i);
+			Max = max(Max, a[i]);
+		}
+		int cnt = 0;
+		int l = 1, r = n;
+		memset(res, -1, sizeof res);
+		while (a[l] < Max) {
+			++cnt;
+			int x = a[l], y = a[l + 1];
+			res[cnt] = pii(x, y);
+			if (x < y) swap(x, y);
+			++l;
+			a[l] = x;
+			a[++r] = y;
+		}
+		for (int i = 1; i <= n; ++i) {
+			b[i] = a[l + i - 1]; 
+		}
+		ll po;
+		while (q--) {
+			scanf("%lld", &po);
+			if (po <= cnt) {
+				printf("%d %d\n", res[po].fi, res[po].se);
+			} else {
+				po -= cnt; 
+				po -= 1;
+				po %= (n - 1);
+				po += 1;
+				printf("%d %d\n", b[1], b[po + 1]);
+			}
+		}
+	}
+	return 0;
+}
---
+++ cf/1179/b.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pii pair <int, int>
+#define fi first
+#define se second
+int n, m;
+
+int main() {
+	while (scanf("%d%d", &n, &m) != EOF) {
+		vector <pii> res;
+		int mid = n / 2;
+		for (int i = 1; i <= mid; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				res.push_back(pii(i, j));
+				res.push_back(pii(n - i + 1, m - j + 1));
+			}
+		}	
+		if (n & 1) {
+			int mmid = m / 2;
+			for (int j = 1; j <= mmid; ++j) {
+				res.push_back(pii(mid + 1, j));
+				res.push_back(pii(mid + 1, m - j + 1));
+			}
+			if (m & 1) {
+				res.push_back(pii(mid + 1, mmid + 1));
+			}
+		}
+		for (auto it : res) {
+			printf("%d %d\n", it.fi, it.se);
+		}
+	//	map <pii, int> mp, mp2;
+	//	for (int i = 1, sze = (int)res.size(); i < sze; ++i) {
+	//		pii t = pii(res[i].fi - res[i - 1].fi, res[i].se - res[i - 1].se);
+	//		pii t2 = pii(res[i].fi, res[i].se);
+	//		if (mp.find(t) != mp.end() || mp2.find(t2) != mp2.end()) {
+	//			cout << "no\n";
+	//			break;
+	//		}
+	//		mp[t] = 1;
+	//		mp2[t2] = 1;
+	//	}
+	}
+	return 0;
+}
