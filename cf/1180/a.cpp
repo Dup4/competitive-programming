deab0bdc721ea6a8ea575a819c16e191dc085296
[Dup4] <[panlvzhi@foxmail.com]>
2019-06-23 12:58:02 +0800

    up

---
+++ cf/1180/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+ll f[220];
+
+int main() {
+	f[1] = 1;
+	ll base = 0;
+	for (int i = 2; i <= 100; ++i) {
+		base += 4;
+		f[i] = f[i - 1] + base;
+	}
+	int n; 
+	while (cin >> n) {
+		cout << f[n] << "\n";
+	}
+	return 0;
+}
