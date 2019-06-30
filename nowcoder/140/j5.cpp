d1552b5c265f5f02a4d90cdf1185cd4788cf5c44
[Dup4] <[panlvzhi@foxmail.com]>
2019-06-30 10:31:39 +0800

    up

---
+++ nowcoder/140/j5.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 4000010
+#define y1 y_1
+int x1[N], y1[N], x2[N], y2[N], k[N];
+int n, m, q;
+int a[N], b[N], c[N];
+bool die[N];
+int id(int x, int y) {
+	return (x - 1) * (m + 2) + y;
+}
+void up(int *f, int x1, int y1, int x2, int y2, int v) {
+	f[id(x1, y1)] += v;
+	f[id(x2 + 1, y2 + 1)] += v;
+	f[id(x1, y2 + 1)] -= v;
+	f[id(x2 + 1, y1)] -= v;
+}
+void work(int *f) {
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			f[id(i, j)] += f[id(i - 1, j)] + f[id(i, j - 1)] - f[id(i - 1, j - 1)];
+		}
+	}
+}
+
+int main() {
+	while (scanf("%d%d%d", &n, &m, &q) != EOF) {
+		memset(b, 0, sizeof b);
+		for (int i = 1; i <= n; ++i) {  
+			for (int j = 1; j <= m; ++j) {
+				scanf("%d", &a[id(i, j)]);
+			}
+		}		
+		for (int i = 1; i <= q; ++i) { 
+			scanf("%d%d%d%d%d", x1 + i, y1 + i, x2 + i, y2 + i, k + i);
+			up(b, x1[i], y1[i], x2[i], y2[i], 1);
+		}
+		work(b);  
+		for (int i = 15; i >= 0; --i) {
+			for (int _i = 1; _i <= n; ++_i) {
+				for (int _j = 1; _j <= m; ++_j) {
+					c[id(_i, _j)] = 0;
+				}
+			}
+			for (int j = 1; j <= q; ++j) {
+				if (k[j] >> i & 1) {
+					up(c, x1[j], y1[j], x2[j], y2[j], 1);
+				}
+			}
+			work(c);
+			for (int _i = 1; _i <= n; ++_i) {
+				for (int _j = 1; _j <= m; ++_j) {
+					if (a[id(_i, _j)] >> i & 1) {
+						if (b[id(_i, _j)] != c[id(_i, _j)]) {
+							die[id(_i, _j)] = 1;
+						}				 
+					} else if (c[id(_i, _j)]) {
+						die[id(_i, _j)] = 1;
+					}
+				}
+			}
+		}
+		int res = 0;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				res += die[id(i, j)];
+			}
+		}
+		printf("%d\n", res);
+	}
+	return 0;
+}
---
+++ nowcoder/140/jj.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 15500010
+int prime[1000010], tot;
+bool check[N];
+void init() {
+	tot = 0;
+	memset(check, 0, sizeof check);
+	for (int i = 2; i < N; ++i) {
+		if (!check[i]) {
+			prime[++tot] = i;
+			if (tot >= 1000000) break;
+		}
+		for (int j = 1; j <= tot; ++j) {
+			if (1ll * i * prime[j] >= N) break;
+			check[i * prime[j]] = 1;
+			if (i % prime[j] == 0) {
+				break;
+			}
+		}
+	}
+}
+int n, m, q;
+vector <vector<int>> a, c;
+vector <vector<ll>> b;
+template <class T>
+void up(vector <vector<T>> &vec, int x1, int y1, int x2, int y2, int v) {
+	vec[x1][y1] += v;
+	vec[x2 + 1][y2 + 1] += v;
+	vec[x1][y2 + 1] -= v;
+	vec[x2 + 1][y1] -= v;
+}
+template <class T>
+void work(vector <vector<T>> &vec) {
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			vec[i][j] += vec[i - 1][j] + vec[i][j - 1] - vec[i - 1][j - 1];
+		}
+	}
+}
+
+int main() {
+	init();
+	cout << tot << endl;
+	random_shuffle(prime + 1, prime + 1 + tot);
+	while (scanf("%d%d%d", &n, &m, &q) != EOF) {
+		a.clear(); a.resize(n + 2, vector <int> (m + 2, 0));
+		b.clear(); b.resize(n + 2, vector <ll> (m + 2, 0));
+		c.clear(); c.resize(n + 2, vector <int> (m + 2, 0));
+		for (int i = 1; i <= n; ++i) {  
+			for (int j = 1; j <= m; ++j) {
+				scanf("%d", &a[i][j]); 
+			}
+		}		
+		int x[2], y[2], k;
+		while (q--) {
+			scanf("%d%d%d%d%d", x, y, x + 1, y + 1, &k);
+			up(b, x[0], y[0], x[1], y[1], prime[k]);
+			up(c, x[0], y[0], x[1], y[1], 1);
+		}
+		work(b); work(c); 
+		int res = 0;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				if (b[i][j] != c[i][j] * prime[a[i][j]]) {
+					++res;
+				}
+			}
+		}
+		printf("%d\n", res);
+	}
+	return 0;
+}
