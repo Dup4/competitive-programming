2308e01e817fcf0a6aa469a41378d6407dc94455
[Dup4] <[panlvzhi@foxmail.com]>
2019-07-13 09:42:06 +0800

    up

---
+++ cf/1191/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	char *s = "DACB";
+	while (scanf("%d", &x) != EOF) {
+		int tot = 0; char S = 'Z';
+		for (int i = 0; i <= 2; ++i) {
+			if (s[(x + i) % 4] < S) {
+				tot = i;
+				S = s[(x + i) % 4];
+			}
+		}
+		printf("%d %c\n", tot, S);
+	}
+	return 0;
+}
