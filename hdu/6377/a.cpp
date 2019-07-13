2ebc1f66b5b93562ec6d9a80c2a11d028dc775dd
[Dup4] <[panlvzhi@foxmail.com]>
2019-07-13 10:29:39 +0800

    up

--- hdu/6377/a.cpp
+++ hdu/6377/a.cpp
@@ -2,58 +2,38 @@
 using namespace std;
 
 #define ll long long
-#define N 6010
-#define M 6000
+#define N 1010
 const ll p = 998244353;
+int n, D;
+//f[n][i]表示在第2n个位置，有i对情侣相邻的方案数
+ll f[N][N];
 void add(ll &x, ll y) {
 	x += y;
 	if (x >= p) x -= p;
 }
-int n;
-ll f[N], g[N], fac[N], inv[N], D;
-ll qmod(ll base, ll n) {
-	ll res = 1;
-	while (n) {
-		if (n & 1) res = res * base % p;
-		base = base * base % p;
-		n >>= 1;
-	}
-	return res;
-}
-ll C(int n, int m) {
-	if (n < m) return 0;
-	if (m == 0) return 1; 
-	return fac[n] * inv[n - m] % p * inv[m] % p;
-}
-
-ll F(int x) {
-	if (f[x] != -1) return f[x];
-	g[0] = fac[2 * x];
-	for (int i = 1; i <= x; ++i) g[i] = C(x, i) * C(x, i) % p * fac[i] % p * qmod(2, i) % p * fac[2 * (x - i)] % p;
-	ll res = 0, A;
-	for (int i = 0; i <= x; ++i) {
-		if (i & 1) A = (p - g[i]) % p;
-		else A = g[i];
-		add(res, A);
-	}
-	return f[x] = res;
-}
 
 int main() {
-	memset(f, -1, sizeof f);
-	fac[0] = 1;
-	for (int i = 1; i <= M; ++i) fac[i] = fac[i - 1] * i % p;
-	inv[M] = qmod(fac[M], p - 2);
-	for (int i = M; i >= 1; --i) inv[i - 1] = inv[i] * i % p; 
-	while (scanf("%d%lld", &n, &D) != EOF) {
+	memset(f, 0, sizeof f);
+	f[1][1] = 2;
+	for (int i = 2; i <= 1000; ++i) {
+		for (int j = 0; j <= i; ++j) {
+			//情侣看成一个点插入
+			if (j) add(f[i][j], f[i - 1][j - 1] * (2 * i - 1 - (j - 1)) % p * 2 % p);
+			add(f[i][j], f[i - 1][j] * j % p * 2 % p);
+			//情侣看成两个点插入
+			add(f[i][j], f[i - 1][j] * (2 * i - 1 - j) % p * (2 * i - 1 - j - 1) % p);
+			if (j < i - 1) add(f[i][j], f[i - 1][j + 1] * (j + 1) % p * (2 * i - 1 - (j + 1)) % p * 2 % p);
+			if (j < i - 2) add(f[i][j], f[i - 1][j + 2] * (j + 2) % p * (j + 1) % p);
+		}
+	}
+	while (scanf("%d%d", &n, &D) != EOF) {
 		ll DD = 1;
 		ll res = 0;
 		for (int i = 0; i <= n; ++i) {
-			add(res, DD * C(n, i) % p * C(n, i) % p * fac[i] % p * qmod(2, i) % p * F(n - i) % p);
+			add(res, DD * f[n][i] % p);
 			DD = DD * D % p;
-		}	
+		}
 		printf("%lld\n", res);
 	}
 	return 0;
 }
-
