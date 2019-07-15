2a451e22ece33e020c6a232036a470432afb2b63
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-07-15 13:00:13 +0800

    up

---
+++ zoj/2019summer/day5/c1.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 100010
+int n, q, k, m, w[N];
+vector <vector<int>> G[2];
+int a[N << 2], Sta[N << 2], vis[N];
+ll f[N][2];
+int fa[N], deep[N], sze[N], son[N], top[N], in[N], out[N], cnt_in;
+void DFS(int u) {
+	sze[u] = 1;
+	for (auto v : G[0][u]) if (v != fa[u]) {
+		deep[v] = deep[u] + 1;
+		fa[v] = u;
+		DFS(v);
+		sze[u] += sze[v];
+		if (!son[u] || sze[v] > sze[son[u]]) {
+			son[u] = v;
+		}	
+	}
+}
+void gettop(int u, int tp) {
+	top[u] = tp;
+	in[u] = ++cnt_in; 
+	if (!son[u]) {
+		out[u] = cnt_in;
+		return;
+	}
+	gettop(son[u], tp);
+	for (auto v : G[0][u]) if (v != fa[u] && v != son[u]) {
+		gettop(v, v);
+	}
+	out[u] = cnt_in;
+}
+int query(int u, int v) {
+	while (top[u] != top[v]) {
+		if (deep[top[u]] < deep[top[v]]) {
+			swap(u, v);
+		}
+		u = fa[top[u]];
+	}
+	if (deep[u] > deep[v]) swap(u, v);
+	return u;
+}
+
+// 0 不选
+// 1 选
+void dp(int u, int pre) {
+	f[u][0] = f[u][1] = 0;
+	if (vis[u]) { 
+		f[u][1] = w[u]; 
+	} 
+	for (auto v : G[1][u]) {
+		if (v != pre) {
+			dp(v, u);
+			f[u][0] += max(f[v][0], f[v][1]);
+			if (fa[v] == u) {
+				f[u][1] += f[v][0];  
+			} else {
+				f[u][1] += max(f[v][0], f[v][1]);
+			}
+		}
+	}
+}
+
+void init() {
+	cnt_in = 0; fa[1] = 0; deep[1] = 0;
+	G[0].clear(); G[0].resize(n + 1); 
+	G[1].clear(); G[1].resize(n + 1);
+	for (int i = 1; i <= n; ++i) son[i] = 0, vis[i] = 0;
+}
+int main() {
+	scanf("%d%d", &n, &q);
+	init();
+	for (int i = 1; i <= n; ++i) {
+		scanf("%d", w + i);
+	}
+	for (int i = 1, u, v; i < n; ++i) {
+		scanf("%d%d", &u, &v);
+		G[0][u].push_back(v);
+		G[0][v].push_back(u);
+	}
+	DFS(1); gettop(1, 1);
+	while (q--) {
+		scanf("%d%d", &k, &m);
+		a[0] = m;
+		for (int i = 1; i <= m; ++i) {
+			scanf("%d", a + i);
+			vis[a[i]] = 1;
+		}
+		sort(a + 1, a + 1 + m, [&](int x, int y) {
+			return in[x] < in[y];		
+		});
+		for (int i = m; i > 1; --i) {
+			a[++a[0]] = query(a[i], a[i - 1]);
+		}
+		sort(a + 1, a + 1 + a[0], [&](int x, int y){
+			return in[x] < in[y];		
+		});
+		a[0] = unique(a + 1, a + 1 + a[0]) - a - 1; 
+		for (int i = 1, top = 0; i <= a[0]; ++i) {
+			while (top && out[Sta[top]] < in[a[i]]) {	
+				--top;
+			}
+			if (top) {
+				G[1][Sta[top]].push_back(a[i]);
+				G[1][a[i]].push_back(Sta[top]);
+			}
+			Sta[++top] = a[i];
+		}
+		dp(a[1], a[1]);
+		printf("%lld\n", max(f[a[1]][0], f[a[1]][1]));
+		for (int i = 1; i <= a[0]; ++i) {
+			vis[a[i]] = 0;
+			G[1][a[i]].clear();
+			f[a[i]][0] = f[a[i]][1] = 0;
+		}
+	}
+	return 0;
+}
---
+++ zoj/2019summer/day5/d.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long 
+#define ll long long 
+#define pii pair <int, int>
+#define fi first
+#define se second 
+#define N 200010
+int n, q, a[N];
+map <ull, int> mp;
+ull base[N], Hashsum; 
+
+struct SEG {
+	struct node {
+		int a, ls, rs;
+		node() {
+			ls = rs = -1;
+		}
+		node (int a, int ls, int rs) : a(a), ls(ls), rs(rs) {}
+	}t[N * 50]; 
+	int rt[N * 10], tot;
+	void init() {
+		rt[0] = 0;
+		tot = 0;  
+	}
+	void build(int &id, int l, int r) {
+		id = ++tot;
+		if (l == r) {
+			t[id] = node();  
+			t[id].a = a[l];
+			return;
+		}
+		int mid = (l + r) >> 1;
+		build(t[id].ls, l, mid);
+		build(t[id].rs, mid + 1, r);
+	}
+	void update(int &now, int pre, int l, int r, int pos, int x) {
+		now = ++tot;
+		t[now] = t[pre];
+		if (l == r) {
+			t[now].a = x;
+			return;
+		}
+		int mid = (l + r) >> 1;
+		if (pos <= mid) update(t[now].ls, t[pre].ls, l, mid, pos, x);
+		else update(t[now].rs, t[pre].rs, mid + 1, r, pos, x);
+	}
+	int query(int id, int l, int r, int pos) {
+		if (l == r) return t[id].a;
+		int mid = (l + r) >> 1;
+		if (pos <= mid) return query(t[id].ls, l, mid, pos);
+		else return query(t[id].rs, mid + 1, r, pos);
+	}
+}seg;
+
+
+int main() {
+	ull tmp = 31;
+	Hashsum = 0;
+	for (int i = 1; i <= 100000; ++i) {
+		base[i] = tmp;
+		tmp *= 31;
+	}
+	while (scanf("%d%d", &n, &q) != EOF) {
+		mp.clear(); seg.init();
+		for (int i = 1; i <= n; ++i) scanf("%d", a + i);
+		for (int i = 1; i <= n; ++i) {
+			Hashsum = base[i] * a[i];
+		}
+		seg.build(seg.rt[0], 1, n);  
+		int P = 1, st = 1, ed = 1;  
+		mp[Hashsum] = 1; 	
+		while (1) {
+			int it = 1;
+			vector <pii> vec; 
+			while (1) {
+				vec.push_back(pii(it, a[it]));  
+				int ls = it * 2, rs = it * 2 + 1;
+				if (ls > n) break; 
+				if (a[ls] > a[rs]) {
+					it = ls;
+				} else {
+					it = rs;
+				}
+			}
+			for (auto it : vec) {
+				Hashsum -= base[it.fi] * it.se;
+			}
+			int sze = (int)vec.size();
+			for (int i = 0; i < sze - 1; ++i) {
+				a[vec[i].fi] = vec[i + 1].se;
+			}
+			a[vec.end()[-1].fi] = vec[0].se;
+			for (auto it : vec) {
+				Hashsum += base[it.fi] * a[it.fi];
+			}
+			if (mp.find(Hashsum) != mp.end()) {
+				st = mp[Hashsum], ed = P; 
+				break;
+			}
+			seg.rt[P] = seg.rt[P - 1];
+			for (auto it : vec) {
+				seg.update(seg.rt[P], seg.rt[P], 1, n, it.fi, a[it.fi]);
+			}
+			++P;
+			mp[Hashsum] = P; 
+		}
+		ll m; int x;
+		for (int i = 1; i <= q; ++i) {
+			scanf("%lld%d", &m, &x);
+			++m;
+			if (m <= st) {
+				printf("%d%c", seg.query(seg.rt[m - 1], 1, n, x), " \n"[i == q]);
+			} else {
+				m -= st;
+				m %= (ed - st + 1);
+				printf("%d%c", seg.query(seg.rt[st + m - 1], 1, n, x), " \n"[i == q]);
+				
+			}
+		}
+	}
+	return 0;
+}
