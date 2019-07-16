091b36c37fe18b930037c8996f09051cf9171fd0
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-07-16 17:48:34 +0800

    up

--- zoj/2019summer/practice/1028/a.cpp
+++ zoj/2019summer/practice/1028/a.cpp
@@ -8,24 +8,30 @@ using namespace std;
 #define N 1010
 int n, m, q, s;
 char G[N][N];
-pll a[27][N][N], base[N][N]; 
+pll a[30][N][N], b[30][N][N], base[N][N], num[N][N]; 
 
-void add(int id, int x1, int y1, int x2, int y2) {
-	a[id][x2][y2].fi += 1;
-	a[id][x2][y2].se += id;
-	a[id][x1][y1].fi += 1;
-	a[id][x1][y1].se += id;
-	a[id][x1 - 1][y2].fi -= 1;
-	a[id][x1 - 1][y2].se -= id;
-	a[id][x2][y1 - 1].fi -= 1;
-	a[id][x2][y1 - 2].se -= id;
+//每一块的贡献 = 原图的贡献 - 覆盖区域的原图的贡献 + 覆盖区域的贡献
+void add(pll a[][N], int id, int x1, int y1, int x2, int y2, ll f) {
+	a[x2 + 1][y2 + 1].fi += f;
+	a[x2 + 1][y2 + 1].se += f * id;
+	a[x1][y1].fi += f;
+	a[x1][y1].se += f * id;
+	a[x1][y2 + 1].fi -= f; 
+	a[x1][y2 + 1].se -= f * id;
+	a[x2 + 1][y1].fi -= f;
+	a[x2 + 1][y1].se -= f * id;
 }
 
 pll query(pll a[][N], int x1, int y1, int x2, int y2) {
 	pll res = pll(0, 0);
-	res.fi = a[x2][y2].fi + a[x1][y1].fi - a[x1 - 1][y2].fi - a[x2][y1 - 1].fi;
-	res.se = a[x2][y2].se + a[x1][y1].se - a[x1 - 1][y2].se - a[x2][y1 - 1].se;
-	return res;  
+	res.fi = a[x2][y2].fi + a[x1 - 1][y1 - 1].fi - a[x1 - 1][y2].fi - a[x2][y1 - 1].fi;
+	res.se = a[x2][y2].se + a[x1 - 1][y1 - 1].se - a[x1 - 1][y2].se - a[x2][y1 - 1].se;
+	return res;   
+}
+
+void addpre(pll a[][N], int x, int y) {
+	a[x][y].fi += a[x - 1][y].fi + a[x][y - 1].fi - a[x - 1][y - 1].fi;
+	a[x][y].se += a[x - 1][y].se + a[x][y - 1].se - a[x - 1][y - 1].se;
 }
 
 struct qnode {
@@ -34,54 +40,71 @@ struct qnode {
 	qnode() {}
 	void scan() {
 		scanf("%d%d%d%d %s", x, y, x + 1, y + 1, c);
-		add(c[0] - 'a', x[0], y[0], x[1], y[1]);
+		add(a[c[0] - 'a' + 1], c[0] - 'a' + 1, x[0], y[0], x[1], y[1], 1);
+		add(num, c[0] - 'a' + 1, x[0], y[0], x[1], y[1], 1);  
 	}
 }qrr[300010];
 
 int main() {
 	while (scanf("%d%d%d%d", &n, &m, &q, &s) != EOF) {
 		memset(a, 0, sizeof a);
-		for (int i = 1; i <= n; ++i) scanf("%s", G[i] + 1);
+		memset(b, 0, sizeof b);  
+		memset(base, 0, sizeof base);
+		memset(num, 0, sizeof num);  
+		for (int i = 1; i <= n; ++i) scanf("%s", G[i] + 1); 
 		for (int i = 1; i <= q; ++i) {
-			qrr[i].scan();
+			qrr[i].scan(); 
 		}
-		for (int i = 0; i < 26; ++i) {
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				addpre(base, i, j);
+				add(b[G[i][j] - 'a' + 1], G[i][j] - 'a' + 1, i, j, i, j, q - num[i][j].fi);
+			}
+		}
+		for (int i = 1; i <= 26; ++i) {
 			for (int j = 1; j <= n; ++j) {
 				for (int k = 1; k <= m; ++k) {
-					a[i][j][k].fi += a[i][j - 1][k].fi + a[i][j][k - 1].fi - a[i][j - 1][k - 1].fi;
-					a[i][j][k].se += a[i][j - 1][k].se + a[i][j][k - 1].se - a[i][j - 1][k - 1].se;
+					addpre(a[i], j, k);
+					addpre(b[i], j, k);
 				}
 			}
 			for (int j = 1; j <= n; ++j) {
 				for (int k = 1; k <= m; ++k) {
-					a[i][j][k].fi += a[i][j - 1][k].fi + a[i][j][k - 1].fi - a[i][j - 1][k - 1].fi;
-					a[i][j][k].se += a[i][j - 1][k].se + a[i][j][k - 1].se - a[i][j - 1][k - 1].se;
+					addpre(a[i], j, k);
+					addpre(b[i], j, k);
 				}
 			}
 			for (int j = 1; j <= n; ++j) {
 				for (int k = 1; k <= m; ++k) {
 					a[i][j][k].fi += a[i - 1][j][k].fi;
 					a[i][j][k].se += a[i - 1][j][k].se;
+					b[i][j][k].fi += b[i - 1][j][k].fi;
+					b[i][j][k].se += b[i - 1][j][k].se;
 				}
 			}
 		}
 		ll tot = 0;
 		for (int i = 1; i <= n; ++i) {
 			for (int j = 1; j <= m; ++j) {
-				int id = G[i][j] - 'a';
-				base[i][j].fi = 1ll * id * a[id - 1][i][j].fi - a[id - 1][i][j].se + a[id + 1][i][j].se - 1ll * id * a[id + 1][i][j].fi;
-				tot += base[i][j].fi;
-				base[i][j].fi += base[i - 1][j].fi + base[i][j - 1].fi - base[i - 1][j - 1].fi;
+				int id = G[i][j] - 'a' + 1;
+				pll l = query(a[id - 1], i, j, i, j);
+				pll r = query(a[id + 1], i, j, i, j);
+				base[i][j].fi = 1ll * id * l.fi - l.se + r.se - 1ll * id * r.fi;
+				tot += base[i][j].fi;  
+				addpre(base, i, j);
 			}
 		}
 		ll Min = 1e18; int pos = -1;
 		for (int i = 1; i <= q; ++i) {
 			int x[2] = {qrr[i].x[0], qrr[i].x[1]};
 			int y[2] = {qrr[i].y[0], qrr[i].y[1]};
-			int id = qrr[i].c[0] - 'a';
+			int id = qrr[i].c[0] - 'a' + 1;
 			ll now = tot - query(base, x[0], y[0], x[1], y[1]).fi;
 		   	pll l = query(a[id - 1], x[0], y[0], x[1], y[1]);
 			pll r = query(a[id + 1], x[0], y[0], x[1], y[1]);
+			now += 1ll * id * l.fi - l.se - 1ll * id * r.fi + r.se;
+			l = query(b[id - 1], x[0], y[0], x[1], y[1]);
+			r = query(b[id + 1], x[0], y[0], x[1], y[1]);
 			now += 1ll * id * l.fi - l.se - 1ll * id * r.fi + r.se;
 			if (now < Min) {
 				Min = now;
