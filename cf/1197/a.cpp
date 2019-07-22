6b021bbaaf9eb84fbd692d3df6f1eab8bdb683d7
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-07-23 00:46:07 +0800

    up

---
+++ cf/1197/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 100010
+int n, a[N];
+
+int main() {
+	int T; scanf("%d", &T);
+	while (T--) {
+		scanf("%d", &n);
+		for (int i = 1; i <= n; ++i) scanf("%d", a + i);
+		sort(a + 1, a + 1 + n);
+		int res = 0;
+		for (int i = 2; i < n; ++i) {
+			res = max(res, min(a[i] - 1, i - 1));
+		}
+		printf("%d\n", res);
+	}
+	return 0;
+}
---
+++ cf/1197/c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define N 300010
+int n, k, a[N];
+
+int main() {
+	while (scanf("%d%d", &n, &k) != EOF) {
+		for (int i = 1; i <= n; ++i) {
+			scanf("%d", a + i);
+		}
+		ll res = a[n] - a[1]; 
+		vector <int> vec;
+		for (int i = 1; i < n; ++i) {
+			vec.push_back(a[i] - a[i + 1]);
+		}
+		sort(vec.begin(), vec.end());
+		for (int i = 0; i < k - 1; ++i) {
+			res += vec[i];
+		}
+		printf("%lld\n", res);
+	}
+	return 0;
+}
