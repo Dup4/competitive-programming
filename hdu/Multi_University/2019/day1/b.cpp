99be0a76f1912b0d63d056e6ab5330892751ec86
[Dup4] <[panlvzhi@foxmail.com]>
2019-07-22 21:46:30 +0800

    up

---
+++ hdu/Multi_University/2019/day1/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 500010
+int n, m;
+int a[N];
+
+int main() {
+	while (scanf("%d%d", &n, &m) != EOF) {
+		for (int i = 1; i <= n; ++i) scanf("%d", a + i);
+		
+	}
+	return 0;
+}
