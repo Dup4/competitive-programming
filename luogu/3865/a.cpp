e9c598c703c879ab6a905516a872c112bb02cb6e
[Dup4] <[panlvzhi@foxmail.com]>
2019-07-28 17:24:01 +0800

    up

--- luogu/3865/a.cpp
+++ luogu/3865/a.cpp
@@ -6,17 +6,18 @@ using namespace std;
 int n, m, a[N];
 
 struct RMQ {
-	int dp[N][M];
+	int Max[N][M], Min[N][M]; 
 	int mm[N];
 	void init(int n, int *b) {
 		mm[0] = -1;
 		for (int i = 1; i <= n; ++i) {
 			mm[i] = ((i & (i - 1)) == 0) ? mm[i - 1] + 1 : mm[i - 1];
-			dp[i][0] = b[i];
+			Max[i][0] = b[i];
+			Min[i][0] = b[i];
 		}
 		for (int j = 1; j <= mm[n]; ++j) {
 			for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
-				dp[i][j] = max(dp[i - 1][j], dp[i + (1 << (j - 1))][j - 1]);
+				dp[i][j] = max(f[i - 1][j], f[i + (1 << (j - 1))][j - 1]);
 			}
 		}
 	}
