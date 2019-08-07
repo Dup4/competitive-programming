57ca54bd847132ccdf7afc65fc193e28ac7669f3
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-08-07 23:57:40 +0800

    up

---
+++ cf/1202/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 100010
+char s[N], t[N];
+
+int main() {
+	int T; scanf("%d", &T);
+	while (T--) {
+		scanf("%s%s", s + 1, t + 1);
+		int len = strlen(s + 1);
+		
+	}
+	return 0;
+}
--- hdu/Multi_University/2019/day6/d3.cpp
+++ hdu/Multi_University/2019/day6/d3.cpp
@@ -96,65 +96,73 @@ struct node {
 	}
 }a[N];
 int n;
-frac x, res;
+frac res, one, zero;
 ll sum[2];
+priority_queue <node, vector <node>, less <node>> A;
+priority_queue <node, vector <node>, greater <node>> B;
+
+frac getres() {
+	frac res = frac(1e9, 1);
+	if (sum[0] == sum[1]) {
+		res = min(res, frac(sum[0], 1));
+	} 
+	if (!A.empty()) {
+		node tmp = A.top(); 
+		sum[0] -= tmp.a;
+		frac x = frac(sum[1] - sum[0] + tmp.b, tmp.a + tmp.b);
+		if (x >= zero && x <= one) {
+			res = min(res, x * frac(tmp.a, 1) + frac(sum[0], 1));
+		}
+		sum[0] += tmp.a;
+	}
+	if (!B.empty()) {
+		node tmp = B.top();
+		sum[1] -= tmp.b;
+		frac x = frac(sum[1] - sum[0] + tmp.b, tmp.a + tmp.b);
+		if (x >= zero && x <= one) {
+			res = min(res, x * frac(tmp.a, 1) + frac(sum[0], 1));
+		}
+		sum[1] += tmp.b;
+	}
+	return res;
+}
 
 int main() {
-	frac one = frac(1, 1), zero = frac(0, 1);  
+	one = frac(1, 1), zero = frac(0, 1);  
 	int T; scanf("%d", &T);
 	while (T--) {
 		scanf("%d", &n);
 		for (int i = 1; i <= n; ++i) a[i].scan(); 
-		priority_queue <node, vector <node>, less <node>> A;
-		priority_queue <node, vector <node>, greater <node>> B;
+		while (!A.empty()) A.pop();
+		while (!B.empty()) B.pop();
 		sum[0] = sum[1] = 0; 
 		for (int i = 1; i <= n; ++i) {
 			A.push(a[i]);
-			sum[0] += a[i].a;  
+			sum[0] += a[i].a; 	
 			if (i == 1) {
-				res = frac(a[i].a * a[i].b, a[i].a + a[i].b);
+				res = frac(a[i].a * a[i].b, a[i].a + a[i].b); 
 			} else {
-				while (1) {
-					if (sum[0] == sum[1]) {
-						res = frac(sum[0], 1);
-						break; 
-					} else if (sum[0] < sum[1]) {
-						node tmp = B.top(); 
+				res = frac(1e9, 1);
+				if (sum[0] == sum[1]) {
+					res = frac(sum[0], 1);
+				} else if (sum[0] < sum[1]) {
+					while (sum[0] < sum[1]) {
+						res = min(res, getres());
+						node tmp = B.top(); B.pop();
 						sum[1] -= tmp.b;
-						if (sum[1] == sum[0] + tmp.a) {
-							B.pop();
-							sum[0] += tmp.a; A.push(tmp);
-							res = frac(sum[0], 1);
-							break;
-						}
-						frac x = frac(sum[1] - sum[0] + tmp.b, tmp.a + tmp.b);
-						if (x >= zero && x <= one) {
-							res = x * frac(tmp.a, 1) + frac(sum[0], 1);
-							sum[1] += tmp.b;
-							break;
-						} else {
-							B.pop(); 
-							A.push(tmp); sum[0] += tmp.a;
-						}
-					} else {
-						node tmp = A.top();
-						sum[0] -= tmp.a;
-						if (sum[0] == sum[1] + tmp.b) {
-							A.pop();
-							B.push(tmp); sum[1] += tmp.b;
-							res = frac(sum[0], 1);
-							break;
-						}
-						frac x = frac(sum[1] - sum[0] + tmp.b, tmp.a + tmp.b);
-						if (x >= zero && x <= one) {
-							res = x * frac(tmp.a, 1) + frac(sum[0], 1);
-							sum[0] += tmp.a; 
-							break;
-						} else {
-							A.pop();
-							B.push(tmp); sum[1] += tmp.b;
-						}
+						sum[0] += tmp.a;
+						A.push(tmp);	
 					}
+					res = min(res, getres());
+				} else {
+					while (sum[0] > sum[1]) {
+						res = min(res, getres());
+						node tmp = A.top(); A.pop();
+						sum[0] -= tmp.a;
+						sum[1] += tmp.b;
+						B.push(tmp);
+					}
+					res = min(res, getres());
 				}
 			}
 			res.print();
