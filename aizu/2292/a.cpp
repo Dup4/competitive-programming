34679c4795d71348c486474c9f99205bdf347227
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-08-09 23:53:49 +0800

    up

--- aizu/2292/a.cpp
+++ aizu/2292/a.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 5e3 + 10;
+const int N = 5e5 + 10;
+#define ll long long
 #define ALP 26
 int n;
-char s[N];
+char s[N], t[N];
 struct PAM{   
 	//每个节点代表一个本质不同的回文串
 	struct node {
@@ -13,11 +14,12 @@ struct PAM{
 		//节点i表示的回文串的长度
 		//节点i表示的回文串在整串中的出现次数
 		//以节点i最后一个字符结尾的回文串的长度和
-		int num, len, cnt, sum;
+		int cnt, vis, len;
 		node(int _len = 0) {
 			memset(Next, 0, sizeof Next);
 			fail = 0;
-			num = cnt = sum = 0;
+			cnt = 0;
+			vis = 0;
 			len = _len;
 		}
 	}t[N];	
@@ -50,10 +52,9 @@ struct PAM{
         while(s[n-t[x].len-1] != s[n]) x = t[x].fail;
         return x;
     }
-    bool add(int c){
-		bool F = 0;
+	void add(int c){
 		//设置偏移量
-        c -= 'a';
+        c -= 'A';
         s[++n] = c;
 		//通过上一个回文串找这个回文串的匹配位置
 		int cur = get_fail(last);
@@ -62,23 +63,43 @@ struct PAM{
             int now = newnode(t[cur].len + 2);//新建节点
 			//如果这个回文串没有出现过，说明出现了一个新的本质不同的回文串
             t[now].fail = t[get_fail(t[cur].fail)].Next[c];
-            t[cur].Next[c] = now;
-            t[now].num = t[t[now].fail].num + 1;
-            t[now].sum = t[t[now].fail].sum + t[now].len;
-			F = 1;
+            t[cur].Next[c] = now;            
 		}
         last = t[cur].Next[c];
 		++t[last].cnt;
         //返回的当前字符加入后是否新增了一个本质不同的回文串
-		return F;
 		//那么此时的p - 2就表示加入这个字符后共有多少个本质不同的回文串
     }
-    void count(){
-        // 最后统计一遍每个节点出现个数
-        // 父亲累加儿子的cnt,类似SAM中parent树
-        // 满足parent拓扑关系
+    void gao(char *s){
+		for (int i = 1, len = strlen(s + 1), now = 1; i <= len; ++i) {
+			int c = s[i] - 'A';
+			while (now != 1 && (!t[now].Next[c] || s[i] != s[i - t[now].len - 1]))
+				now = t[now].fail;
+			if (t[now].Next[c] && s[i] == s[i - t[now].len - 1]) {
+				now = t[now].Next[c];
+				++t[now].vis;
+			} else {
+				now = 1;
+			}
+		}
+		ll res = 0;
 		for (int i = p - 1;  i >= 0; --i) {
 			t[t[i].fail].cnt += t[i].cnt;
+			t[t[i].fail].vis += t[i].vis;
+			if (i > 1)
+			res += 1ll * t[i].vis * t[i].cnt;
 		}
+		printf("%lld\n", res);
     }
 }pam;
+
+int main() {
+	while (scanf("%s%s", s + 1, t + 1) != EOF) {
+		pam.init();
+		for (int i = 1, len = strlen(s + 1); i <= len; ++i) {
+			pam.add(s[i]);
+		}
+		pam.gao(t);
+	}
+	return 0;
+}
