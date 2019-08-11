f12ed64c99b38ab0d8674edb771b8e9792135d7f
[Dup4] <[panlvzhi@foxmail.com]>
2019-08-11 14:12:59 +0800

    up

--- jisuanke/3258/e.cpp
+++ jisuanke/3258/e.cpp
@@ -6,35 +6,27 @@ const ll D = 1e18;
 const int p = 1e6 + 7; 
 const int N = 3e4 + 10;
 int n, q;
+string w[N];
 struct Num {
 	ll a[3], v; 
 	Num() {
 		a[0] = a[1] = a[2] = 0;
 		v = 0;
 	}
-	void scan() {
-		string s; 
-		cin >> s;
+	Num(string s) {
 		v = 0;
-	    reverse(s.begin(), s.end());	
+		reverse(s.begin(), s.end()); 
 		while (s.size() < 54) s += "0";
 		reverse(s.begin(), s.end());
-		cout << s << endl;
+	//	cout << s << endl;
+		a[0] = a[1] = a[2] = 0;
 		for (int i = 0; i < 54; ++i) {
 			a[i / 18] = a[i / 18] * 10 + s[i] - '0';
 			a[i / 18] %= p;
 		}
 		for (int i = 0; i < 3; ++i) {
-			v = v * (D % p) % p + (a[i] % p);
+			v = v * (D % p) % p + (a[i] % p);  
 			v %= p;	
-		}
-	}
-	void rev() {
-		reverse(a, a + 3);
-		v = 0;
-		for (int i = 0; i < 3; ++i) {
-			v = v * (D % p) % p + (a[i] % p);
-			v %= p;
 		}
 	}
 	bool operator < (const Num &other) const {
@@ -44,7 +36,7 @@ struct Num {
 		}
 		return 1;
 	}
-}w[N];
+};
 vector <vector<int>> G;
 
 int fa[N], deep[N], sze[N], son[N], top[N], in[N], fin[N], out[N], cnt;
@@ -80,29 +72,36 @@ void gettop(int u, int tp) {
 
 struct SEG {
 	struct node {
-		Num x, y;
-		int lazy;
+		Num x[3]; 
+		int lazy, add;
 		node() {
-			x = Num(); y = Num();
-			lazy = 0;
+			for (int i = 0; i < 3; ++i)
+				x[i] = Num();
+			lazy = 0; add = 0;
 		}
-		void add(int v) {
-			if (v) swap(x, y);
-			lazy ^= v;
+		void up() {
+			swap(x[0], x[1]); 
+			lazy ^= 1; 
 		}
 		node operator + (const node &other) const {
 			node res = node();
-			res.x = max(x, other.x);
-			res.y = max(y, other.y);
-			return res;
+			for (int i = 0; i < 3; ++i)
+				res.x[i] = max(x[i], other.x[i]);
+			res.add = add and other.add;  
+			return res; 
 		}
 	}t[N << 2], res;
 	void build(int id, int l, int r) {
 		t[id] = node();
 		if (l == r) {
-			t[id].x = w[fin[l]];
-			t[id].y = t[id].x;
-			t[id].y.rev();
+			string &s = w[fin[l]]; 
+			t[id].x[0] = Num(s); 
+			reverse(s.begin(), s.end());
+			t[id].x[1] = Num(s);
+			reverse(s.begin(), s.end()); 
+			while (s.size() > 1 && s.back() == '0') s.pop_back(); 
+			t[id].x[2] = Num(s); 
+		//	cout << t[id].x.v << " " << t[id].y.v << " " << t[id].z.v << endl; 
 			return;
 		}
 		int mid = (l + r) >> 1;
@@ -112,14 +111,24 @@ struct SEG {
 	void pushdown(int id) {
 		int &lazy = t[id].lazy;
 		if (lazy) {
-			t[id << 1].add(lazy);
-			t[id << 1 | 1].add(lazy);
+			t[id << 1].up();
+			t[id << 1 | 1].up();
 			lazy = 0;
 		}
 	}
 	void update(int id, int l, int r, int ql, int qr) {
-		if (l >= ql && r <= qr) {
-			t[id].add(1);
+		if (l == r) {
+			if (t[id].add == 0) {
+				t[id].add = 1;
+				t[id].x[0] = t[id].x[1];
+				t[id].x[1] = t[id].x[2];
+			} else {
+				t[id].up(); 
+			}
+			return;
+		}
+		if (t[id].add && l >= ql && r <= qr) {
+			t[id].up();
 			return;
 		}
 		int mid = (l + r) >> 1;
@@ -170,7 +179,7 @@ int main() {
 		cnt = 0;
 		for (int i = 1; i <= n; ++i) son[i] = 0;
 		for (int i = 1; i <= n; ++i) {
-			w[i].scan();
+			cin >> w[i];
 		}
 		for (int i = 1, u, v; i < n; ++i) {
 			cin >> u >> v;
@@ -191,7 +200,7 @@ int main() {
 				cin >> v;
 				seg.res = SEG::node();
 				query(u, v);
-				cout << seg.res.x.v << "\n";
+				cout << seg.res.x[0].v << "\n";
 			}
 		}
 	}	
