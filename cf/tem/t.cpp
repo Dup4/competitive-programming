6fd47b94d5b44f091d0fb9e77762aebfccf415ab
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-08-15 07:08:09 +0800

    up

--- cf/tem/t.cpp
+++ cf/tem/t.cpp
@@ -8,7 +8,7 @@ template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; if (x >= 
 template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
 inline int rd() { int x; cin >> x; return x; }
-inline void out(char *s) { cout << s << "\n"; }
+template <class T > inline void out(T s) { cout << s << "\n"; }
 inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
 int n;
@@ -19,6 +19,7 @@ void run() {
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
 	while (cin >> n) run();
 	return 0;
 }
