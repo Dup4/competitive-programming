3d4057caa91ac77d045e3db71f4c82aa24cb9f51
[Dup4] <[panlvzhi@foxmail.com]>
2019-08-21 20:45:02 +0800

    up

--- opentrains/1434/a.cpp
+++ opentrains/1434/a.cpp
@@ -8,7 +8,7 @@ const int N = 5e4 + 10;
 int n;
 char s[N][10];
 pii res[100000 + 10]; int sze;
-bitset <N> b[5][76], empty, all, now; 
+bitset <N> b[5][76], now; 
 
 void gao() {
 	sze = 0;
@@ -21,34 +21,19 @@ void gao() {
 		}
 	}
 	for (int i = 2; i <= n; ++i) { 
-	    now.reset();	
-		int limit = i - 1;  
+	    now.reset(); 	
 		for (int j = 0; j < 5; ++j) {
 			now |= b[j][s[i][j] - 48];
 		}
-		int pre = 1;
-		while (pre <= limit) { 
-			int l = pre, r = limit, pos = -1;
-			while (r - l >= 0) {
-				int mid = (l + r) >> 1;
-				if (((all >> (N - mid)) ^ (now & (all >> (N - mid)))).count() > 0) { 
-					pos = mid;
-					r = mid - 1;
-				} else {
-					l = mid + 1; 
-				}
-			}
-			if (pos == -1) break;
-			now |= (all >> (N - pos)); 
-			res[++sze] = pii(pos, i);
-			if (sze >= 100000) return; 
-			pre = pos + 1;	
-		}
+		now = ~now;
+	    for (int it = now._Find_first(); it < i - 1; it = now._Find_next(it)) {
+			res[++sze] = pii(it + 1, i);
+			if (sze >= 100000) return;
+		} 	
 	}
 }
 
 int main() {
-	all.reset(); all.flip(); empty.reset(); 
 	while (scanf("%d", &n) != EOF) {
 		for (int i = 1; i <= n; ++i) scanf("%s", s[i]);  
 		gao();
