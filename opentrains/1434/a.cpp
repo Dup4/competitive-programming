16ef054703d2cde52bcba5888744e7fd1351a6d7
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-08-21 17:27:57 +0800

    up

--- opentrains/1434/a.cpp
+++ opentrains/1434/a.cpp
@@ -7,31 +7,25 @@ using namespace std;
 const int N = 5e4 + 10; 
 int n;
 char s[N][10];
-vector <pii> res;
-bitset <N> b[5][76], empty, one, all, now; 
+pii res[100000 + 10]; int sze;
+bitset <N> b[5][76], empty, all, now; 
 
 void gao() {
-	all.reset(); one.reset(); empty.reset(); one[0] = 1;
-	for (int i = 0; i < N; ++i) all[i] = 1;
+	sze = 0;
 	for (int i = 0; i < 5; ++i) 
 		for (int j = 0; j < 76; ++j)
-			b[i][j] = b[i][j] & empty;
+			b[i][j].reset();
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 1; j <= n; ++j) {  
 			b[i][s[j][i] - 48][j - 1] = 1; 
 		}
 	}
-	for (int i = n; i >= 1; --i) { 
-	    now = now & empty;	
+	for (int i = 2; i <= n; ++i) { 
+	    now.reset();	
 		int limit = i - 1;  
 		for (int j = 0; j < 5; ++j) {
 			now |= b[j][s[i][j] - 48];
 		}
-	//	cout << i << endl;
-	//	for (int j = n - 1; j >= 0; --j) {
-	//		cout << now[j];
-	//	}
-	//	cout << endl;
 		int pre = 1;
 		while (pre <= limit) { 
 			int l = pre, r = limit, pos = -1;
@@ -46,25 +40,20 @@ void gao() {
 			}
 			if (pos == -1) break;
 			now |= (all >> (N - pos)); 
-		//	cout << i << " " << pos << endl;
-		//	for (int j = n - 1; j >= 0; --j) {
-		//		cout << now[j];
-		//	}
-		//	cout << endl;
-			res.push_back(pii(pos, i));
-			if (res.size() >= 100000) return; 
+			res[++sze] = pii(pos, i);
+			if (sze >= 100000) return; 
 			pre = pos + 1;	
 		}
 	}
 }
 
 int main() {
+	all.reset(); all.flip(); empty.reset(); 
 	while (scanf("%d", &n) != EOF) {
 		for (int i = 1; i <= n; ++i) scanf("%s", s[i]);  
 		gao();
-		int sze = res.size();
 		printf("%d\n", sze);
-		for (int i = 0; i < sze; ++i)
+		for (int i = 1; i <= sze; ++i)
 			printf("%d %d\n", res[i].fi, res[i].se);
 	}
 	return 0;
