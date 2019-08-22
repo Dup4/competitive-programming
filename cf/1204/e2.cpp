e714f2895e694a9724b4c07c4f61188c42809378
[Dup4] <[panlvzhi@foxmail.com]>
2019-08-22 20:36:29 +0800

    up

--- cf/1204/e2.cpp
+++ cf/1204/e2.cpp
@@ -2,34 +2,48 @@
 using namespace std;
 #define debug(...) { printf("#  "); printf(__VA_ARGS__); puts(""); }
 #define ll long long
-const int N = 5e3 + 10;
-const int mod = 998244353;
+const int N = 1e6 + 10;
+const ll mod = 998244853;
 template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; if (x >= mod) x -= mod; } 
 template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
 inline int rd() { int x; cin >> x; return x; }
 template <class T > inline void out(T s) { cout << s << "\n"; }
 inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+inline ll qpow(ll base, ll n) { 
+	ll res = 1; 
+	while (n) { 
+		if (n & 1) res = res * base % mod; 
+		base = base * base % mod; 
+		n >>= 1; 
+	} 
+	return res; 
+}
 int n, m;
 ll fac[N], inv[N];
 ll C(int n, int m) {
-	if (n < m || m < 0) return 0; 
-	return fac[n] * inv[n - m] % mod * inv[m] % mod;
+	if (m < 0 || m > n) return 0; 
+	return fac[n] * inv[m] % mod * inv[n - m] % mod;  
 }
 void run() {
-	ll res = 0, base = 0;
-	if (n > m) {
-		base = n - m;
-		n = m;
+	ll res = 0, pre = 0, now;
+	if (n <= m) {
+		for (int i = 0; i <= n; ++i) {
+			now = C(n + m, n) - C(n + m, n - 1 - i);
+			res = res + (now - pre) * i % mod;
+			res = res % mod;
+			pre = now;
+		}	
+	} else {
+		for (int i = 0; i <= m; ++i) {
+			now = C(n + m, m) - C(n + m, m - 1 - i);
+			res = res + (now - pre) * (i + n - m) % mod;
+			res = res % mod;
+			pre = now;
+		}
 	}
-	ll pre = 0;
-	for (int i = 0; i <= n; ++i) {
-		ll now = (C(n + m, n) - C(n + m, n - 1 - i) + mod) % mod;
-		chadd(res, 1ll * i * (now - pre + mod) % mod);
-		pre = now;
-	}
-	printf("%lld\n", res);
+	res = (res + mod) % mod;
+	cout << res << "\n";
 }
 
 int main() {
