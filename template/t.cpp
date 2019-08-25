f08c7feef3681635b52da140a8f97c815b3314ed
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-08-26 07:14:14 +0800

    up

--- template/t.cpp
+++ template/t.cpp
@@ -1,48 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define debug(...) { printf("#  "); printf(__VA_ARGS__); puts(""); }
-#define ll long long
+#define fi first
+#define se second
 #define endl "\n" 
-constexpr int N = 1e3 + 10;
+using ll = long long;
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+using VI = vector <int>;
+using VL = vector <ll>;
 constexpr int mod = 1e9 + 7;
 template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; if (x >= mod) x -= mod; else if (x < 0) x += mod; } 
 template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
 template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
 inline int rd() { int x; cin >> x; return x; }
 template <class T> inline void rd(T &x) { cin >> x; }
-template <class T > inline void out(T s) { cout << s << "\n"; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }
+template <class T> inline void out(T s) { cout << s << "\n"; }
+template <class T> inline void out(vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
 inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
-int n, a[N][N];
-bool check() {
-	int res = -1;
-	for (int i = 1; i <= n; ++i) {
-		int num = 0;
-		for (int j = 1; j <= n; ++j) {
-			num ^= a[i][j];
-		}
-		if (res == -1) res = num;
-		else if (num != res) return 0;
-		num = 0;
-		for (int j = 1; j <= n; ++j) {
-			num ^= a[j][i];
-		}
-		if (num != res) return 0;
-	}
-	return 1;
-}
+constexpr int N = 1e5 + 10;
+int n; 
 void run() {
-	int cnt = 0;
-	for (int i = 1; i <= n; i += 4)
-		for (int j = 1; j <= n; j += 4) 
-			for (int p = 0; p < 4; ++p)
-				for (int q = 0; q < 4; ++q)
-					a[i + p][j + q] = cnt++;
-		
-	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= n; ++j)
-			cout << a[i][j] << " \n"[j == n];
-	assert(check());
+
 }
 
 int main() {
