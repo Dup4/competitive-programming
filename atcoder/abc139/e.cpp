af81e3e560f5eb53ed22c9c23c40d90fcffd0a44
[Dup4] <[panlvzhi@foxmail.com]>
2019-09-01 21:39:17 +0800

    up

---
+++ atcoder/abc139/e.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(...) { printf("#  "); printf(__VA_ARGS__); puts(""); }
+#define fi first
+#define se second
+#define endl "\n" 
+using ll = long long;
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+using VI = vector <int>;
+using VL = vector <ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; if (x >= mod) x -= mod; else if (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }
+template <class T> inline void out(T s) { cout << s << "\n"; }
+template <class T> inline void out(vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+constexpr int N = 1e3 + 10;
+int n, used[N];
+vector <vector<int>> a;
+int gao() {
+	bool F = 0;
+	memset(used, 0, sizeof used);
+	for (int i = 1; i <= n; ++i) if (!used[i] && !a[i].empty()) { 
+		int v = a[i].back();
+		if (!used[v] and a[v].back() == i) {
+			used[i] = 1;
+			used[v] = 1;
+			a[i].pop_back();
+			a[v].pop_back();
+			F = 1;
+		}
+	}
+	return F;
+}
+bool done() {
+	for (int i = 1; i <= n; ++i)
+		if (!a[i].empty())
+			return 0;
+	return 1;
+}
+void run() {
+	a.clear(); a.resize(n + 1);
+	for (int i = 1; i <= n; ++i) {
+		a[i].clear(); a[i].resize(n - 1);
+		for (auto &it : a[i]) cin >> it;
+		reverse(a[i].begin(), a[i].end());
+	}
+	int res = 0;
+   // cout << gao() << endl;	
+	while (1) {
+		if (!gao()) break; 
+		++res;
+	}
+	if (not done()) res = -1;
+	cout << res << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	while (cin >> n) run();
+	return 0;
+}
---
+++ jisuanke/3004/h.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+using ll = long long;
+
+const int N = 310;
+const ll INFLL = 0x3f3f3f3f3f3f3f3f;
+
+struct Edge {
+    int v;
+    ll w;
+
+    Edge(int _v = 0, ll _w = 0) : v(_v), w(_w) {}
+};
+
+int n, m;
+vector<vector<Edge>> G;
+bool vis[N];
+int cnt[N];
+ll dis[N];
+
+bool SPFA(int s, int t, int w) {
+    G[s].push_back(Edge(t, w));
+    for (int i = 0; i <= n; ++i) {
+        vis[i] = false;
+        cnt[i] = 0;
+        dis[i] = INFLL;
+    }
+    vis[s] = true;
+    dis[s] = 0;
+    cnt[s] = 1;
+    queue<int> q;
+    q.push(s);
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        vis[u] = false;
+        for (auto e: G[u]) {
+            int v = e.v;
+            ll cost = e.w;
+            if (dis[v] > dis[u] + cost) {
+                dis[v] = dis[u] + cost;
+                if (!vis[v]) {
+                    vis[v] = true;
+                    q.push(v);
+                    if (++cnt[v] > n) {
+                        G[s].pop_back();
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    G[s].pop_back();
+    return true;
+}
+
+void RUN() {
+    int _T;
+    scanf("%d", &_T);
+    while (_T--) {
+        scanf("%d %d", &n, &m);
+        G.clear();
+        G.resize(n + 1);
+        for (int i = 1, u, v, w; i <= m; ++i) {
+            scanf("%d %d %d", &u, &v, &w);
+            G[u].push_back(Edge(v, w));
+        }
+        for (int _q = 1, s, t; _q <= 6; ++_q) {
+            scanf("%d %d", &s, &t);
+            int l = -1e9, r = 1e9, res = 1e9;
+            while (r - l >= 0) {
+                int mid = (l + r) >> 1;
+                if (SPFA(s, t, mid)) {
+                    r = mid - 1;
+                    res = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            G[s].push_back(Edge(t, res));
+            printf("%d\n", res);
+        }
+    }
+}
+
+int main() {
+#ifdef LOCAL_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    RUN();
+
+#ifdef LOCAL_JUDGE
+//    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+    return 0;
+}
