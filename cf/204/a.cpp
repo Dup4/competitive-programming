6254769b20f6410ea04e7f07158dccd528cb0e35
[Dup4] <[panlvzhi@foxmail.com]>
2019-09-11 07:50:00 +0800

    up

---
+++ cf/204/a.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int N = 1e5 + 10;
+const int ALP = 26;
+int n, k; 
+char s[N];
+
+struct SAM {
+	//空间开两倍，节点个数是两倍字符串长度
+	//maxlen 表示节点i表示的最大后缀长度, nx[j]表示节点i加一个字符j所表示的字符串对应的节点
+	//minlen 表示节点i表示的最小后缀长度，其等于t[t[i].fa].maxlen + 1 
+	//fa 表示节点i的后缀链接 cnt 表示节点i的enspos集合大小 
+	//c[]表示拓扑排序辅助数组,　rk[]表示拓扑序，rk[i],i小的节点所表示的后缀长度也小
+	//pos表示那个结点在字符串的endpos的最小值，即firstpos
+	struct node {
+		int maxlen, cnt, fa, nx[ALP];
+		node() { maxlen = cnt = fa = 0; memset(nx, 0, sizeof nx); }
+	}t[N << 1];
+	int tot, lst, c[N << 1], rk[N << 1];  
+	inline int newnode() { 
+		++tot;
+		t[tot] = node();
+		return tot;
+	}
+	inline void init() {
+		tot = 0;
+		lst = newnode();
+	}
+	inline void extend(int id) { 
+		int cur = newnode(), p; 
+		t[cur].cnt = 1;
+		t[cur].maxlen = t[lst].maxlen + 1; 
+		for (p = lst; p && !t[p].nx[id]; p = t[p].fa) t[p].nx[id] = cur;
+		if (!p) {
+			t[cur].fa = 1;
+		} else {
+			int q = t[p].nx[id];
+			if (t[q].maxlen == t[p].maxlen + 1) {
+				t[cur].fa = q;
+			} else {
+				int clone = newnode();
+			  	t[clone] = t[q]; t[clone].cnt = 0;
+				t[clone].maxlen = t[p].maxlen + 1; 
+				for (; p && t[p].nx[id] == q; p = t[p].fa) t[p].nx[id] = clone;
+				t[cur].fa = t[q].fa = clone;
+			}
+		}
+		lst = cur; 	
+	}
+	//字符串从0开始 
+	void build(char *s) { 
+		init();
+		for (int i = 0; s[i]; ++i) {
+			extend(s[i] - 'a');
+		}
+		memset(c, 0, sizeof c); 
+		for (int i = 1; i <= tot; ++i) c[t[i].maxlen]++;
+		for (int i = 1; i <= tot; ++i) c[i] += c[i - 1];
+		for (int i = 1; i <= tot; ++i) rk[c[t[i].maxlen]--] = i;
+		for (int i = tot; i; --i) t[t[rk[i]].fa].cnt += t[rk[i]].cnt;
+	}
+	void gao() {
+		ll res = 0;
+		for (int i = 1; i <= tot; ++i) if (t[i].cnt >= k) {
+			res += t[i].maxlen - t[t[i].fa].maxlen;
+		}
+		printf("%lld\n", res);
+	}
+}sam;
+
+int main() {
+	while (scanf("%d%d", &n, &k) != EOF) {
+		scanf("%s", s);
+		sam.build(s); sam.gao();
+	}
+	return 0;
+}
