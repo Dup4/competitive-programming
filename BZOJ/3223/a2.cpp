635bb4e5d738a685d0fa49ee67fc7856807f2c22
[Dup4] <[panlvzhi@foxmail.com]>
2019-09-11 16:37:57 +0800

    up

---
+++ BZOJ/3223/a2.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+int n, q;
+struct FHQ {
+	#define ls t[x].son[0]
+	#define rs t[x].son[1]
+	struct node {
+		int son[2], key, rnd, sze, rev;   
+	}t[N];
+	int tot, rt;
+	inline void init() { tot = rt = 0; }
+	inline void pushup(int x) {
+		t[x].sze = t[ls].sze + t[rs].sze + 1;
+		if (x && t[x].rev) {
+			t[x].rev ^= 1;
+			swap(t[x].son[0], t[x].son[1]);
+			t[ls].rev ^= 1;
+			t[rs].rev ^= 1;
+		}
+	}
+	inline int random() {
+		static int seed = 703;
+		return seed = int(seed * 482147ll % 2147483647);
+	}
+	inline int newnode(int key) {
+		++tot;
+		t[tot].sze = 1;
+		t[tot].key = key;
+		t[tot].rnd = random(); 
+		t[tot].rev = t[tot].son[0] = t[tot].son[1] = 0;
+		return tot;
+	}
+	int build(int l, int r) {
+		if (l > r) return 0;
+		int mid = (l + r) >> 1, v = mid - 1;
+		int x = newnode(v);
+		ls = build(l, mid - 1);
+		rs = build(mid + 1, r);
+		pushup(x);
+		return x;
+	}
+	int merge(int x, int y) {
+		if (!x || !y) return x + y;
+		pushup(x); pushup(y);
+		if (t[x].rnd < t[y].rnd) {
+			t[x].son[1] = merge(t[x].son[1], y);
+			pushup(x);
+			return x;
+		} else {
+			t[y].son[0] = merge(x, t[y].son[0]);
+			pushup(y);
+			return y;
+		}
+	}
+	//根据sze分
+	inline void split(int now, int k, int &x, int &y) {
+		if (!now) x = y = 0;
+		else {
+			pushup(now);
+			if (k <= t[t[now].son[0]].sze) {
+				y = now;
+				split(t[now].son[0], k, x, t[now].son[0]);
+			} else {
+				x = now;
+				split(t[now].son[1], k - t[t[now].son[0]].sze - 1, t[now].son[1], y);
+			}
+			pushup(now);
+		}
+	}
+	inline void rev(int l, int r) {
+		int a, b, c, d;
+		split(rt, r + 1, a, b);
+		split(a, l, c, d);
+		t[d].rev ^= 1;
+		rt = merge(merge(c, d), b);
+	}
+	inline void print(int x) {
+		if (!x) return;
+		pushup(x);
+		print(ls);
+		if (t[x].key >= 1 && t[x].key <= n) printf("%d ", t[x].key);
+		print(rs);
+	}
+}fhq;
+
+int main() {
+	fhq.init();
+	scanf("%d%d", &n, &q);	
+	fhq.rt = fhq.build(1, n + 2);
+	for (int i = 1, l, r; i <= q; ++i) {
+		scanf("%d%d", &l, &r);
+		fhq.rev(l, r);
+	}
+	fhq.print(fhq.rt);
+	return 0;
+}
---
+++ luogu/3835/a.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#define R register
+#define W while
+#define IN inline
+#define gc getchar()
+#define MX 500005
+static bool fu;
+template <class T>
+IN void in(T &x)
+{
+    fu = false; R char c = gc; x = 0;
+    W (!isdigit(c))
+    {if(c == '-') fu = true; c = gc;}
+    W (isdigit(c))
+    {x = (x << 1) + (x << 3), x += c - 48, c = gc;}
+    if(fu) x = -x;
+}
+namespace fhqTreap
+{
+    struct Node
+    {
+        int son[2], val, siz, pri;
+    }tree[MX * 50];
+    int root[MX];
+    int cnt;
+    IN int randomm()
+    {
+        static int seed = 703; 
+        return seed = int(seed * 48271LL % 2147483647);
+    }
+    IN void pushup(const int &now)
+    {tree[now].siz = tree[tree[now].son[0]].siz + tree[tree[now].son[1]].siz + 1;}
+    IN int new_node (const int &giv)
+    {
+        tree[++cnt].val = giv;
+        tree[cnt].siz = 1;
+        tree[cnt].pri = randomm();
+        return cnt;
+    }
+    int merge (int x, int y)
+    {
+        if(!x || !y) return x + y;
+        if(tree[x].pri > tree[y].pri)
+        {
+            int now = ++cnt; tree[now] = tree[x]; 
+            tree[now].son[1] = merge(tree[now].son[1], y);
+            pushup(now); return now;
+        }
+        else
+        {
+            int now = ++cnt; tree[now] = tree[y];
+            tree[now].son[0] = merge(x, tree[now].son[0]);
+            pushup(now); return now;
+        }
+    }
+    void split(int now, int k, int &x, int &y)
+    {
+        if(!now) {x = y = 0; return;}
+        if(tree[now].val <= k)
+        {
+            x = ++cnt; tree[x] = tree[now];
+            split(tree[x].son[1], k, tree[x].son[1], y);
+            pushup(x);
+        }
+        else
+        {
+            y = ++cnt; tree[y] = tree[now];
+            split(tree[y].son[0], k, x, tree[y].son[0]);
+            pushup(y);
+        }
+    }
+    void Del(int &root, int tar)
+    {
+        int x = 0, y = 0, z = 0;
+        split(root, tar, x, z);
+        split(x, tar - 1, x, y);
+        y = merge(tree[y].son[0], tree[y].son[1]);
+        root = merge(merge(x, y), z);
+    }
+    void insert(int &root, int tar)
+    {
+        int x = 0, y = 0, z = 0;
+        split(root, tar, x, y);
+        root = merge(merge(x, new_node(tar)), y);
+    }
+    int get_val(int now, int tar)
+    {
+        if(tar == tree[tree[now].son[0]].siz + 1) return tree[now].val;
+        else if(tar <= tree[tree[now].son[0]].siz) return get_val(tree[now].son[0], tar);
+        else return get_val(tree[now].son[1], tar - tree[tree[now].son[0]].siz - 1);
+    }
+    IN int kth(int &root, int tar)
+    {
+        int x, y;
+        split(root, tar - 1, x, y);
+        int ans = tree[x].siz + 1;
+        root = merge(x, y);
+        return ans;
+    }
+    IN int pre(int &root, int tar)
+    {
+        int x, y, k, ans;
+        split(root, tar - 1, x, y);
+        if(!x) return -2147483647;
+        k = tree[x].siz;
+        ans = get_val(x, k);
+        root = merge(x, y);
+        return ans;
+    }
+    IN int suc(int &root, int tar)
+    {
+        int x, y, ans;
+        split(root, tar, x, y);
+        if(!y) return 2147483647;
+        else ans = get_val(y, 1);
+        root = merge(x, y);
+        return ans;
+    }
+}
+using namespace fhqTreap;
+int main(void)
+{
+    int q;
+    R int base, com, t;
+    in(q);
+    for (R int i = 1; i <= q; ++i)
+    {
+        in(base), in(com), in(t);
+        root[i] = root[base];
+        switch(com)
+        {
+            case 1:
+            {insert(root[i], t); break;}
+            case 2:
+            {Del(root[i], t); break;}
+            case 3:
+            {printf("%d\n", kth(root[i], t)); break;}
+            case 4:
+            {printf("%d\n", get_val(root[i], t)); break;}
+            case 5:
+            {printf("%d\n", pre(root[i], t)); break;}
+            case 6:
+            {printf("%d\n", suc(root[i], t)); break;}
+        }
+    }
+}
