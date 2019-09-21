39faeb9cf4f22381453a530b1cb92eb6d9420b5c
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-09-22 07:14:52 +0800

    up

---
+++ nowcoder/1087/b.cpp
@@ -0,0 +1,53 @@
+// 2 * 31 * 1847
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(...) { printf("#  "); printf(__VA_ARGS__); puts(""); }
+#define fi first
+#define se second
+#define endl "\n" 
+using ll = long long;
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+using VI = vector <int>;
+using VL = vector <ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }
+template <class T> inline void out(T s) { cout << s << "\n"; }
+template <class T> inline void out(vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+constexpr int N = 1e5 + 10;
+int n;
+map <int, int> mp;
+void run() {
+	mp.clear();
+	for (int i = 1, x; i <= n; ++i) {
+		cin >> x;
+		++mp[x];
+	}
+	ll res = 0;
+	//2 * 31 * 1847
+	res += 1ll * (mp[1] + 1) * mp[2] * mp[31] * mp[1847];
+	//62 * 1847
+	res += 1ll * (mp[1] + 1) * mp[62] * mp[1847];
+	//2 * 57257
+	res += 1ll * (mp[1] + 1) * mp[2] * mp[57257];
+	//31 * 3694
+    res += 1ll * (mp[1] + 1) * mp[31] * mp[3694];	
+	//114514
+	res += 1ll * (mp[1] + 1) * mp[114514];
+	cout << res << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	while (cin >> n) run();
+	return 0;
+}
