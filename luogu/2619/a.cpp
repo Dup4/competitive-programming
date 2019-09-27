5fbb739ea0fbcf17311a6f15245e6b8483a57fdf
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-09-27 12:05:07 +0800

    up

--- luogu/2619/a.cpp
+++ luogu/2619/a.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef double db;
 typedef long long ll;
 const int N = 1e5 + 10;
 const ll INF = 0x3f3f3f3f3f3f3f3f; 
-const db eps = 1e-8;
 struct Edge { 
-	int u, v, w, vis; db k; 
+	int u, v, w, k, vis; 
 	Edge() {}
 	Edge(int u, int v, int w, int vis) : u(u), v(v), w(w), vis(vis) {k = 0;}
 	bool operator < (const Edge &other) const {
-		if (vis == other.vis) return w < other.w;
+		if (k + w == other.k + other.w) return vis > other.vis;
 		return k + w < other.k + other.w;
 	}
 }e[N];
@@ -22,7 +20,24 @@ inline void merge(int u, int v) {
 	fa[u] = v; 
 }
 int n, m, C; 
-int check(db K) {
+int check(int K) {
+	for (int i = 1; i <= m; ++i) {
+		e[i].k = e[i].vis * K; 
+	}
+	sort(e + 1, e + 1 + m);
+	memset(fa, 0, sizeof fa);
+	int cnt_e = 0; ll tot = 0; 
+	for (int i = 1; i <= m; ++i) {
+		int u = e[i].u, v = e[i].v, w = e[i].w;
+		if (find(u) == find(v)) continue;
+		cnt_e += e[i].vis; 
+		merge(u, v);
+		tot += w;
+	} 
+	return cnt_e;
+}
+
+int calc(int K) {
 	for (int i = 1; i <= m; ++i) {
 		e[i].k = e[i].vis * K;
 	}
@@ -35,24 +50,8 @@ int check(db K) {
 		cnt_e += e[i].vis;
 		merge(u, v);
 		tot += w;
-	} 
-	return cnt_e;
-}
-
-int calc(db K) {
-	for (int i = 1; i <= m; ++i) {
-		e[i].k = e[i].vis * K;
 	}
-	sort(e + 1, e + 1 + m);
-	memset(fa, 0, sizeof fa);
-	int cnt_e = 0; ll tot = 0; 
-	for (int i = 1; i <= m; ++i) {
-		int u = e[i].u, v = e[i].v, w = e[i].w;
-		if (find(u) == find(v) || (e[i].vis && cnt_e >= C)) continue;
-		cnt_e += e[i].vis;
-		merge(u, v);
-		tot += w;
-	}
+	assert(cnt_e == C);
 	return tot;
 }
 
@@ -63,14 +62,14 @@ int main() {
 			++u, ++v;
 			e[i] = Edge(u, v, w, vis ^ 1);
 		}
-		db l = -2000, r = 2000, res = -2000;
-		while (fabs(r - l) > eps) {
-			db mid = (l + r) / 2;
-			if (check(mid) <= C) {
-				res = mid;
-				r = mid;
+		int l = -200, r = 100, res = -200;
+		while (r - l >= 0) { 
+			int mid = (l + r) >> 1;
+			if (check(mid) < C) { 
+				r = mid - 1;
 			} else {
-				l = mid;
+				res = mid;
+				l = mid + 1;
 			}
 		}
 		printf("%d\n", calc(res));
