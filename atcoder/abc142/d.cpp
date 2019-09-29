28c1531fc4c86ee769a01afe02e5a3c5e0c046c1
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-09-29 12:05:20 +0800

    up

---
+++ atcoder/abc142/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(...) { printf("#  "); printf(__VA_ARGS__); puts(""); }
+#define fi first
+#define se second
+#define endl "\n" 
+using ll = long long;
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+using VI = vector <int>;
+using VL = vector <ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }
+template <class T> inline void out(T s) { cout << s << "\n"; }
+template <class T> inline void out(vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
+inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+constexpr int N = 1e5 + 10;
+ll A, B; 
+vector <ll> vec;
+void gao(ll x) {
+	for (ll i = 2; i * i <= x; ++i) {
+		if (x % i == 0) vec.push_back(i);
+		while (x % i == 0) x /= i;
+	}
+	if (x != 1) vec.push_back(x); 
+}
+void run() {
+	vec.clear();
+	ll G = gcd(A, B);
+	gao(G);	
+	sort(vec.begin(), vec.end());
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+	cout << vec.size() + 1 << endl;  
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	while (cin >> A >> B) run();
+	return 0;
+}
