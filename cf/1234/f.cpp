b8c60ef72dc35020e5207102d65b53b4cc934fe9
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-10-02 07:24:54 +0800

    up

--- cf/1234/f.cpp
+++ cf/1234/f.cpp
@@ -21,15 +21,15 @@ template <class T> inline void pt(T s) { cout << s << "\n"; }
 template <class T> inline void pt(vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
 inline ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
-constexpr int N = 2e6 + 10, D = (1 << 20) - 1; 
-int n, h[N], vis[N]; char s[N]; 
+constexpr int N = 2e6 + 10; 
+int n, h[N], vis[N], D, a[N]; char s[N]; 
 inline int calc() {
 	int res = 0, mask = 0;
 	for (int i = 1; s[i]; ++i) {
-		int c = s[i] - 'a';
+		int c = a[i];
 		if (((mask >> c) & 1) == 0) {
 			++res;
-			mask |= (1 << c);
+			mask |= (1 << c); 
 		}
 	}
 	return res;
@@ -38,11 +38,16 @@ void run() {
 	n = strlen(s + 1);
 	memset(h, 0, sizeof h); 
 	memset(vis, 0, sizeof vis); 
-	int res = 0, cnt = calc();
+	vector <char> vec;
+	for (int i = 1; s[i]; ++i) vec.push_back(s[i]);
+	sort(vec.begin(), vec.end());
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+	for (int i = 1; i <= n; ++i) a[i] = lower_bound(vec.begin(), vec.end(), s[i]) - vec.begin();
+	int res = 0, cnt = calc(); D = (1 << cnt) - 1; 
 	for (int i = n; i >= 1; --i) {
 		int mask = 0, num = 0;
 		for (int j = i; j >= 1; --j) {
-			int c = s[j] - 'a';
+			int c = a[j];
 			if (((mask >> c) & 1) == 0) {
 				mask |= 1 << c; 
 				++num;
@@ -52,7 +57,7 @@ void run() {
 		if (res == cnt) break;
 		num = 0, mask = 0;
 		for (int j = i; j <= n; ++j) {
-			int c = s[j] - 'a';
+			int c = a[j];
 			if (((mask >> c) & 1) == 0) {
 				mask |= 1 << c;
 				++num;
