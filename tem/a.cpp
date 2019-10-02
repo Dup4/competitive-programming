1188ad20d89e6503f4b16142d873bdaf48d84ad7
[Dup4] <[panlvzhi@foxmail.com]>
2019-10-02 20:26:09 +0800

    up

--- tem/a.cpp
+++ tem/a.cpp
@@ -1,48 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int N = 1e5 + 10;
-struct BIT {
-    ll a[N];
-	inline void init() { memset(a, 0, sizeof a); }
-    inline int lowbit(int x) { return x & -x; }
-    void add(int x, ll v) {
-        for (int i = x; i < N; i += lowbit(i))
-            a[i] += v;
-    }
-    ll query(int x) {
-        ll ret = 0;
-        for (int i = x; i > 0; i -= lowbit(i))
-            ret += a[i];
-        return ret;
-    }
-	//a[i]表示i这个数有a[i]个，相当于权值BIT，查询第k大。
-    int kth(ll k) {
-        int p = 0;
-        for (int lim = 1 << 20; lim; lim /= 2)
-            if (p + lim < N && a[p + lim] < k) {
-                p += lim;
-                k -= a[p];
-            }
-        return p + 1;
-    }
-    ll query(int l, int r) { if (l > r) return 0; return query(r) - query(l - 1); }
-    void add(int l, int r, ll v) { if (l > r) return; add(l, v); add(r + 1, -v); }
-}bit;
 
-int main() {
-	int n, q; cin >> n >> q;
-	bit.init();
-	int op, l, r, v, k, x;
-	while (q--) {
-		cin >> op;
-		if (op == 1) {
-			cin >> x >> v;
-			bit.add(x, v);
-		} else {
-			cin >> k;
-			cout << bit.kth(k) << endl;
-		}
-	}
-	return 0;
+#define MAXN 100000
+
+void insert_numbers(long long x)
+{
+    clock_t timestart=clock();
+    unordered_map<long long,int> numbers;
+    for(int i=1;i<=MAXN;i++)
+        numbers[i*x]=i;
+    long long sum=0;
+    for(auto element:numbers)
+        sum+=(element.first/x)*element.second;
+    printf("x = %lld : %.3lfseconds, sum = %lld\n",x,(double)(clock()-timestart)/CLOCKS_PER_SEC,sum);
+}
+
+int main()
+{
+    insert_numbers(107897);
+    insert_numbers(126271);
 }
--- tem/b.cpp
+++ tem/b.cpp
@@ -1,43 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-const int N = 1e7 + 10;
-const int M = 1e7; 
-int pri[N], check[N];
-ll mu[N], f[N], g[N];
-void sieve() {
-	memset(check, 0, sizeof check);
-	*pri = 0;
-	mu[1] = 1;
-	for (int i = 2; i < N; ++i) {
-		if (!check[i]) {
-			pri[++*pri] = i;
-			mu[i] = -1;
-		}
-		for (int j = 1; j <= *pri; ++j) {
-			if (1ll * i * pri[j] >= N) break;
-			check[i * pri[j]] = 1;
-			if (i % pri[j] == 0) {
-				mu[i * pri[j]] = 0;
-				break;
-			} else mu[i * pri[j]] = -mu[i];
-		}
-	}
-	for (int i = 1; i < N; ++i) f[i] = mu[i];
+#define MAXN 100000
+
+struct custom_hash
+{
+    unsigned long long operator()(unsigned long long x)const
+    {
+        x+=0x9e3779b97f4a7c15;
+        x=(x^(x>>30))*0xbf58476d1ce4e5b9;
+        x=(x^(x>>27))*0x94d049bb133111eb;
+        return x^(x>>31);
+    }
+};
+
+void insert_numbers(long long x)
+{
+    clock_t timestart=clock();
+    unordered_map<__int128,int, custom_hash> numbers;
+    for(int i=1;i<=MAXN;i++)
+        numbers[i*x]=i;
+    long long sum=0;
+    for(auto element:numbers)
+        sum+=(element.first/x)*element.second;
+    printf("x = %lld : %.3lfseconds, sum = %lld\n",x,(double)(clock()-timestart)/CLOCKS_PER_SEC,sum);
 }
 
-void get_g(int N, const ll *f, ll *g) {
-  for (int i = 1; i <= N; i++) g[i] = f[i];
-  for (int i = 1; i <= *pri; i++)
-    for (int j = N / pri[i]; j >= 1; j--)
-      g[j * pri[i]] = (g[j * pri[i]] - g[j]);
-} // Magic! O(nloglogn)
-
-int main() {
-	sieve();
-	get_g(M, f, g);
-//	for (int i = 1; i <= M; ++i)
-//		printf("%lld\n", g[i]);
-	return 0;
+int main()
+{
+    insert_numbers(107897);
+    insert_numbers(126271);
 }
