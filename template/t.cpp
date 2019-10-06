e5b0f413ddb4d8a4a4a59c19445182c4aa8132b8
[Dup4] <[panlvzhi@foxmail.com]>
2019-10-06 21:14:42 +0800

    up

--- template/t.cpp
+++ template/t.cpp
@@ -18,8 +18,8 @@ template <class T> inline void rd(T &x) { cin >> x; }
 template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }
 inline void pt() { cout << endl; }
 template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg << " "; pt(args...); }
-template <class T> inline void pt(T s) { cout << s << "\n"; }
-template <class T> inline void pt(vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
+template <class T> inline void pt(const T &s) { cout << s << "\n"; }
+template <class T> inline void pt(const vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
 constexpr int N = 1e5 + 10;
