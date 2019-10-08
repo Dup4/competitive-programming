6053dd294edd71fb982f04a87b48ca3858b2b77f
[Dup4] <[panlvzhi@foxmail.com]>
2019-10-08 14:54:57 +0800

    up

--- opentrains/1491/comp_k/gen.cpp
+++ opentrains/1491/comp_k/gen.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 mt19937 rnd(time(0));
 int main() {
-	int T = 3; cout << T << endl;
+	int T = 1; cout << T << endl;
 	while (T--) {
 		string s = "", t = "";
-		int n = rnd() % 20 + 1, m = rnd() % 100 + 1;
+		int n = rnd() % 10 + 1, m = rnd() % 5 + 1;
 		for (int i = 1; i <= n; ++i) s += rnd() % 2 + 'a';
 		cout << s << endl;
-		int q = 10;
+		int q = 1;
 		cout << q << endl;
 		for (int i = 1; i <= q; ++i) {
 			m = rnd() % 5 + 1;
--- opentrains/1491/k3.cpp
+++ opentrains/1491/k3.cpp
@@ -5,93 +5,31 @@ using ll = long long;
 using ull = unsigned long long;
 using B = bitset<N>; 
 int q, lens, lent, unit;  
-char s[N], t[N];  
+char s[N], t[N], rs[N], rt[N];  
 B S[26], G, All; 
 vector <B> T;
 
-struct Hash{
-	static ull base[N];
-	static void init() {
-		base[0] = 1;
-		for (int i = 1; i < N; ++i)
-			base[i] = base[i - 1] * 131;
-	}
-	ull a[N];
-	inline void gao(char *s) {
-		a[0] = 0;
-		for (int i = 1; s[i]; ++i)
-			a[i] = a[i - 1] * 131 + s[i];
-	}
-	inline ull get(int l, int r) {
-		return a[r] - a[l - 1] * base[r - l + 1];
-	}
-}hs, ht;
-ull Hash::base[N] = {0};
-
-//struct ExKMP {
-//	int Next[N], extend[N];
-//	void get_Next(char *s) {
-//		int lens = strlen(s + 1), p = 1, pos;
-//		Next[1] = lens;
-//		while (p + 1 <= lens && s[p] == s[p + 1]) ++p;
-//		Next[pos = 2] = p - 1;
-//		for (int i = 3; i <= lens; ++i) {
-//			int len = Next[i - pos + 1];
-//			if (len + i < p + 1) Next[i] = len;
-//			else {
-//				int j = max(p - i + 1, 0);
-//				while (i + j <= lens && s[j + 1] == s[i + j]) ++j;
-//				p = i + (Next[pos = i] = j) - 1;
-//			}
-//		}
-//	}
-//	void work(char *s, char *t) {
-//		get_Next(t);
-//		int lens = strlen(s + 1), lent = strlen(t + 1), p = 1, pos;
-//		while (p <= lent && s[p] == t[p]) ++p; 
-//		p = extend[pos = 1] = p - 1;
-//		for (int i = 2; i <= lens; ++i) {
-//			int len = Next[i - pos + 1];
-//			if (len + i < p + 1) extend[i] = len;
-//			else {
-//				int j = max(p - i + 1, 0);
-//				while (i + j <= lens && j <= lent && t[j + 1] == s[i + j]) ++j;
-//				p = i + (extend[pos = i] = j) - 1;
-//			}
-//		}
-//	}
-//}exkmp;
-
 struct ExKMP {
-	int Next[N];
-	int extend[N];
-	//下标从1开始
+	int Next[N], extend[N];
 	void get_Next(char *s) {
 		int lens = strlen(s + 1), p = 1, pos;
-		//Next[1]要特殊考虑
 		Next[1] = lens;
 		while (p + 1 <= lens && s[p] == s[p + 1]) ++p;
-		//Next[2]初始化
 		Next[pos = 2] = p - 1;
 		for (int i = 3; i <= lens; ++i) {
 			int len = Next[i - pos + 1];
-			//第一种情况
 			if (len + i < p + 1) Next[i] = len;
-			//第二种情况
 			else {
-				//找到对于子串最靠后已经匹配的位置
 				int j = max(p - i + 1, 0);
-				//暴力匹配
 				while (i + j <= lens && s[j + 1] == s[i + j]) ++j;
-				p = i + (Next[pos = i] = j) - 1; 
+				p = i + (Next[pos = i] = j) - 1;
 			}
 		}
 	}
-
 	void work(char *s, char *t) {
 		get_Next(t);
 		int lens = strlen(s + 1), lent = strlen(t + 1), p = 1, pos;
-		while (p <= lent && s[p] == t[p]) ++p;
+		while (p <= lent && s[p] == t[p]) ++p; 
 		p = extend[pos = 1] = p - 1;
 		for (int i = 2; i <= lens; ++i) {
 			int len = Next[i - pos + 1];
@@ -103,29 +41,26 @@ struct ExKMP {
 			}
 		}
 	}
-}exkmp;
+}exkmp[2];
 
 //exkmp + Hash
 inline ll gao1() {
 	if (lent > lens) return 0;
 	if (lent == 1) return lens; 
 	ll res = 0;
-	ht.gao(t);
-	exkmp.work(s, t);  
+	exkmp[0].work(s, t);  
+	exkmp[1].work(rs, rt);
+	reverse(exkmp[1].extend + 1, exkmp[1].extend + 1 + lens);
 	for (int i = 1; i + lent - 1 <= lens; ++i) {
 		int end = i + lent - 1;
-		int nx = i + exkmp.extend[i] + 1;
-		if (nx > end) ++res;
-		else if (hs.get(nx, end) == ht.get(exkmp.extend[i] + 2, lent)) 
-				++res;
-		
+		if (exkmp[0].extend[i] + exkmp[1].extend[end] >= lent - 1) ++res;
 	}
 	return res;
 }
 
 inline ll gao2() {
 	if (lent == 1) return lens;
-	T[lent + 1].set();
+	T[lent + 1] = All; 
 	for (int i = lent; i >= 1; --i) {
 		T[i] = (T[i + 1] >> 1) & S[t[i] - 'a'];
 	}
@@ -139,25 +74,29 @@ inline ll gao2() {
 	return res;
 }
 
+void copy(char *s, char *t) {
+	int len = strlen(s + 1);
+	for (int i = 1; i <= len; ++i) t[i] = s[i];
+	t[len + 1] = 0;
+	reverse(t + 1, t + 1 + len);
+}
+
 int main() {
-	Hash::init();
-//	T.clear(); T.resize(N / 64 + 10);
+	T.clear(); T.resize(N / 64 + 10);
 	int _T; scanf("%d", &_T);
-	while (_T--) {
-		scanf("%s", s + 1); lens = strlen(s + 1); unit = lens / 64; 
-		hs.gao(s);
-	//	for (int i = 0; i < 26; ++i) S[i].reset();
-	//	for (int i = 1; s[i]; ++i) S[s[i] - 'a'][i] = 1;
-	//	All.set(); 
-	//	All = (All << (lens + 1));
-	//	All.flip(); All[0] = 1; All[lens + 1] = 1; 
+	while (_T--) { 
+		scanf("%s", s + 1); lens = strlen(s + 1); unit = lens / 64; copy(s, rs); 
+		for (int i = 0; i < 26; ++i) S[i].reset();
+		for (int i = 1; s[i]; ++i) S[s[i] - 'a'][i] = 1; 
+		All.set(); 
+		All = (All << (lens + 1));
+		All.flip(); All[0] = 1; All[lens + 1] = 1; 
 		scanf("%d", &q); 
 		while (q--) {
-			scanf("%s", t + 1); lent = strlen(t + 1);
+			scanf("%s", t + 1); lent = strlen(t + 1); copy(t, rt);
 			ll res = 0;
-			res = gao1(); 
-		//	if (lent >= unit) res = gao1();
-		//	else res = gao2();
+			if (lent >= unit) res = gao1();
+			else res = gao2();
 			printf("%lld\n", res);
 		}
 	}
