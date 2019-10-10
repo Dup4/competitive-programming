904487a85ec165f66b5a577c82f8d5cb3a2b5f47
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-10-10 12:07:47 +0800

    up

--- template/t.cpp
+++ template/t.cpp
@@ -1,7 +1,6 @@
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include <bits/stdc++.h>
-#define debug(...) { printf("#  "); printf(__VA_ARGS__); puts(""); }
 #define fi first
 #define se second
 #define endl "\n" 
@@ -17,17 +16,23 @@ template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y;
 template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
 inline int rd() { int x; cin >> x; return x; }
 template <class T> inline void rd(T &x) { cin >> x; }
-template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }
-inline void pt() { cout << endl; }
-template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg << " "; pt(args...); }
-template <class T> inline void pt(const T &s) { cout << s << "\n"; }
-template <class T> inline void pt(const vector <T> &vec) { for (auto &it : vec) cout << it << " "; cout << endl; } 
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }  
+#define dbg(x...) do { cout << "\033[32;1m" << #x << " -> "; err(x); } while (0) 
+void err() { cout << "\033[39;0m" << endl; } 
+template <class T, class... Ts> void err(const T& arg, const Ts&... args) { cout << arg << ' '; err(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void err(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; err(args...); }
+inline void pt() { cout << endl; } 
+template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg << ' '; pt(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void pt(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; pt(args...); }
 ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
 inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+//head
 constexpr int N = 1e5 + 10;
 int n; 
 void run() {
-
+	
 }
 
 int main() {
