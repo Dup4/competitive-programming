6d3d4b6176abed836ddf53b127f475ee1e3ee275
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-10-11 09:31:02 +0800

    up

--- hdu/4467/b2.cpp
+++ hdu/4467/b2.cpp
@@ -5,20 +5,17 @@ using pII = pair <int, int>;
 #define fi first
 #define se second
 const int N = 1e5 + 10;
-int n, m, q, a[N], d[N], ee[N][3], big[N], S; ll f[N][2], g[3]; 
+int n, m, q, a[N], d[N], ee[N][3], big[N], bigp[N], S; ll f[N][2], g[3]; 
 struct Edge {int v, nx; ll w; }e[N << 1]; int h[N];
 inline void addedge(int u, int v, ll w) { e[++*h] = {v, h[u], w};  h[u] = *h; }
 inline ll id(int u, int v) { return 1ll * u * n + v; } 
 map <ll, ll> mp;
 void up(int u, int vis) {
-	if (big[u]) {
-		g[a[u]] += f[u][0] * vis;
-		g[a[u] + 1] += f[u][1] * vis;
-	}
 	for (int o = h[u]; o; o = e[o].nx) {
 		int v = e[o].v; ll w = e[o].w;
-		g[a[u] + a[v]] += w * vis; 
-		if (big[u] != big[v]) {
+		if (big[u] == big[v]) {
+			g[a[u] + a[v]] += w * vis;
+		} else {	
 			f[v][a[u]] += w * vis; 
 		}
 	}
@@ -33,7 +30,7 @@ int main() {
 			d[i] = h[i] = 0;
 			f[i][0] = f[i][1] = 0;
 		}
-		memset(g, 0, sizeof g); mp.clear();
+		memset(g, 0, sizeof g); mp.clear(); *bigp = 0;
 		for (int i = 1; i <= n; ++i) scanf("%d", a + i); 
 		for (int i = 1; i <= m; ++i) {
 			scanf("%d%d%d", ee[i], ee[i] + 1, ee[i] + 2);
@@ -43,6 +40,7 @@ int main() {
 		}
 		for (int i = 1; i <= n; ++i) {
 			big[i] = (d[i] >= S); 
+			if (big[i]) bigp[++*bigp] = i;
 		}
 		for (int i = 1; i <= m; ++i) {
 			int u = ee[i][0], v = ee[i][1]; ll &w = mp[id(ee[i][0], ee[i][1])];
@@ -58,24 +56,26 @@ int main() {
 			w = 0;
 		}
 		for (int u = 1; u <= n; ++u) {
-			g[a[u]] += f[u][0];
-			g[a[u] + 1] += f[u][1];
-		}
-		for (int u = 1; u <= n; ++u) {
 			for (int o = h[u]; o; o = e[o].nx) {
 				int v = e[o].v; ll w = e[o].w;
-				if (v > u || big[u] != big[v]) continue;
+				if (v > u || big[u] != big[v]) continue; 
 				g[a[u] + a[v]] += w;
 			}
 		}
-	//	for (int i = 1; i <= n; ++i) cout << i << " " << f[i][0] << " " << f[i][1] << endl;
 		scanf("%d", &q);
 		char op[10]; int x, y;
 		while (q--) {
 			scanf("%s%d", op, &x);
 			if (op[0] == 'A') {
 				scanf("%d", &y);
-				printf("%lld\n", g[x + y]);
+				if (x > y) swap(x, y);
+				ll res = g[x + y];
+				for (int i = 1; i <= *bigp; ++i) {
+					int u = bigp[i];
+					if (a[u] >= x && a[u] <= y)
+						res += f[u][x + y - a[u]];
+				}
+				printf("%lld\n", res);
 			} else {
 				up(x, -1);
 				a[x] ^= 1;
---
+++ hdu/5716/a2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 5e2 + 10;
+using B = bitset<N>;
+int n, m, id[300];
+char s[2000100], t[N];
+B b[70], f; 
+
+void getid() {
+	int tot = 0;
+	for (int i = 'a'; i <= 'z'; ++i) id[i] = ++tot;
+	for (int i = 'A'; i <= 'Z'; ++i) id[i] = ++tot;
+	for (int i = '0'; i <= '9'; ++i) id[i] = ++tot;
+}
+
+int main() {
+	getid();
+	while (gets(s + 1)) { 
+		n = strlen(s + 1);
+		scanf("%d", &m); 
+		for (int i = 1; i < 63; ++i) b[i].reset(); 
+		for (int i = 1, x; i <= m; ++i) {
+			scanf("%d%s", &x, t + 1); 
+			for (int j = 1; j <= x; ++j)
+				b[id[t[j]]][i] = 1;
+		}
+		gets(t);
+		f.reset(); f[0] = 1;
+		int flag = 0;
+		for (int i = 1; i <= n; ++i) {
+			if (id[s[i]]) {
+				f = (f << 1) & b[id[s[i]]];
+			} else {
+				f.reset(); 
+			}
+			f[0] = 1;
+			if (f[m] == 1) flag = 1, printf("%d\n", i - m + 1);
+		}
+		if (!flag) puts("NULL");
+	}
+	return 0;
+}
