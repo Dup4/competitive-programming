50648ca800cffbbcb27b6cf6224732f7b63c3477
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-10-14 08:00:34 +0800

    up

--- opentrains/10394/e.cpp
+++ opentrains/10394/e.cpp
@@ -10,7 +10,7 @@ void sim(ll &a, ll &b) {
 }
 void gao(ll a, ll b, ll c, ll d, ll &x, ll &y) {
 	sim(a, b); sim(c, d);
-	ll nx = a / b + 1, ny = c / d + (c % d > 0) - 1;
+	ll nx = a / b + 1, ny = (c + d - 1) / d - 1;
 	if (nx <= ny) x = nx, y = 1;
 	else if (!a) x = 1, y = d / c + 1;
 	else if (a < b && c <= d) gao(d, c, b, a, y, x);
@@ -27,19 +27,19 @@ int main() {
 	while (_T--) {
 		char s[30];
 		scanf("%s", s);
-		ll r = 0, t = 1, a, b, c, d, x, y;
+		ll r = 0, t = 10, a, b, c, d, x, y;
 		for (int i = 2; s[i]; ++i) r = r * 10 + s[i] - '0';
 		if (!r) {
 			puts("0 1");
 			continue;
 		}
 		for (int i = 1; i <= 18; ++i) t = t * 10;
-		a = r - 1, b = t; sim(a, b);
-		c = r + 1, d = t; sim(c, d);
+		a = r * 10 - 5, b = t; sim(a, b);
+		c = r * 10 + 5, d = t; sim(c, d);
 		gao(a, b, c, d, x, y);
 		sim(x, y);
-	//	if (b < y) x = a, y = b;
-	//	if (d < y) x = c, y = d;
+		if (b < y) x = a, y = b;
+		assert(x <= 1000000000 && y <= 1000000000);
 		pt(x); putchar(' ');
 		pt(y); putchar('\n');
 	}
