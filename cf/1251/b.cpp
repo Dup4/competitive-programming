5a192c924eb08bf1bdcd947502a6b9c5f174f56d
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-10-25 00:33:10 +0800

    up

---
+++ cf/1251/b.cpp
@@ -0,0 +1,96 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define endl "\n" 
+using namespace std;
+using db = double;
+using ll = long long;
+using ull = unsigned long long; 
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }  
+#define dbg(x...) do { cout << "\033[32;1m" << #x << " -> "; err(x); } while (0) 
+void err() { cout << "\033[39;0m" << endl; } 
+template <class T, class... Ts> void err(const T& arg, const Ts&... args) { cout << arg << ' '; err(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void err(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; err(args...); }
+inline void pt() { cout << endl; } 
+template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg << ' '; pt(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void pt(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; pt(args...); }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+//head
+constexpr int N = 1e5 + 10;
+int n, cnt[2], len[110]; 
+void run() {
+	n = rd();
+	cnt[0] = cnt[1] = 0;
+	for (int i = 1; i <= n; ++i) {
+		string s; cin >> s;
+		len[i] = s.size();
+		for (auto &c : s) ++cnt[c - '0'];
+	}
+	sort(len + 1, len + 1 + n);
+	int res = 0;
+	for (int i = 1; i <= n; ++i) {
+		if (len[i] & 1) {
+			if (cnt[0] & 1) {
+				--cnt[0];
+			} else if (cnt[1] & 1) {
+				--cnt[1];
+			} else if (cnt[0]) {
+				--cnt[0];
+			} else if (cnt[1]) {
+				--cnt[1];
+			} else {
+				break;
+			}
+			--len[i];
+		}
+		if (len[i] > cnt[0]) {
+			if (cnt[0] & 1) {
+				len[i] = len[i] - cnt[0] + 1;
+				cnt[0] = 1;
+			} else {
+				len[i] -= cnt[0];
+				cnt[0] = 0;
+			}
+		} else {
+			cnt[0] -= len[i];
+			len[i] = 0;
+		}
+		if (len[i] > cnt[1]) {
+			if (cnt[1] & 1) {
+				len[i] = len[i] - cnt[1] + 1;
+				cnt[1] = 1;
+			} else {
+				len[i] -= cnt[1];
+				cnt[1] = 0;
+			}
+		} else {
+			cnt[1] -= len[i];
+			len[i] = 0;
+		}
+		if (len[i]) break;
+		++res;
+	}
+	pt(res);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	int _T = rd();
+	while (_T--) run();
+	return 0;
+}
