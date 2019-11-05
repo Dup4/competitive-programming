e35b3e8068713f4575986728517042a70195dfe3
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-11-05 10:30:25 +0800

    up

---
+++ cf/1256/e.cpp
@@ -0,0 +1,71 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define endl "\n" 
+using namespace std;
+using db = double;
+using ll = long long;
+using ull = unsigned long long; 
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }  
+#define dbg(x...) do { cout << "\033[32;1m" << #x << " -> "; err(x); } while (0) 
+void err() { cout << "\033[39;0m" << endl; } 
+template <class T, class... Ts> void err(const T& arg, const Ts&... args) { cout << arg << ' '; err(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void err(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; err(args...); }
+inline void pt() { cout << endl; } 
+template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg << ' '; pt(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void pt(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; pt(args...); }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+//head
+constexpr int N = 2e5 + 10;
+int n; pII a[N]; ll f[N]; int pre[N], ans[N]; 
+void run() {
+	for (int i = 1; i <= n; ++i) a[i].fi = rd(), a[i].se = i;
+	sort(a + 1, a + 1 + n, [&](pII x, pII y) { return x.fi < y.fi; });
+	a[0].fi = 0; 
+	f[3] = a[3].fi - a[1].fi; pre[3] = 0;
+	f[4] = a[4].fi - a[1].fi; pre[4] = 0;
+	f[5] = a[5].fi - a[1].fi; pre[5] = 0;
+	int pos = 0; ll Min = -a[1].fi; 
+	for (int i = 6; i <= n; ++i) {
+		int x = i - 3;
+		if (f[x] - a[x + 1].fi < Min) {
+			Min = f[x] - a[x + 1].fi;
+			pos = x;
+		} 
+		pre[i] = pos;
+		f[i] = a[i].fi + Min;
+	}
+	ll res = f[n]; int sze = 0;
+	int it = n;
+	while (it) {
+		++sze;
+		for (int i = pre[it] + 1; i <= it; ++i) {
+			ans[a[i].se] = sze;
+		}
+		it = pre[it];
+	}
+	pt(res, sze);
+	for (int i = 1; i <= n; ++i) 
+		cout << ans[i] << " \n"[i == n];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	while (cin >> n) run();
+	return 0;
+}
---
+++ hihocoder/1236/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 5e4 + 10;
+int n, m, q;
+bitset <500001>b[N][5]; 
+
+int main() {
+	int _T; scanf("%d", &_T);
+	while (_T--) {
+	
+	}
+	return 0;
+}
