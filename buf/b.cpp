229170e9ba4dc25dbb5908953fc567c350ceefe5
[Dup4] <[panlvzhi@foxmail.com]>
2019-12-08 15:30:47 +0800

    up

--- buf/b.cpp
+++ buf/b.cpp
@@ -3,7 +3,14 @@ using namespace std;
 #define N 10000010
 unsigned int x;
 
+struct E {
+	int x;
+	E() {}
+	bool operator < (E &other) {
+		return x < other.x;
+	}
+};
+
 int main(int argc, char *argv[]) { 
-	system("time ./a");	
 	return 0;
 }
