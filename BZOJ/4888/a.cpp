6ca901ecfb0e2835c9afd02e56512cb57260a142
[Dup4] <[panlvzhi@foxmail.com]>
2019-12-10 20:18:15 +0800

    up

---
+++ BZOJ/4888/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, cnt[30][2];
+
+int main() {
+	while (scanf("%d", &n) != EOF) {
+		memset(cnt, 0, sizeof cnt);
+		
+		for (int i = 1; i <= n; ++i) {
+		
+		}
+	}
+	return 0;
+}
