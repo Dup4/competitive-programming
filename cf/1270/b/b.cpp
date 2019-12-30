42d1ee67852d3f584420410b4b0e3db03144360e
Dup4 <38343778+Dup4@users.noreply.github.com>
2019-12-30 08:04:11 +0800

    up

---
+++ cf/1270/b/b.cpp
@@ -0,0 +1,124 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define endl "\n" 
+using namespace std;
+using db = double;
+using ll = long long;
+using ull = unsigned long long; 
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }  
+#define dbg(x...) do { cout << "\033[32;1m" << #x << " -> "; err(x); } while (0) 
+void err() { cout << "\033[39;0m" << endl; } 
+template <class T, class... Ts> void err(const T& arg, const Ts&... args) { cout << arg << ' '; err(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void err(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; err(args...); }
+void ptt() { cout << endl; }
+template <class T, class... Ts> void ptt(const T& arg, const Ts&... args) { cout << ' ' << arg; ptt(args...); }
+template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg; ptt(args...); }
+void pt() {}
+template <template<typename...> class T, typename t, typename... A>
+void pt(const T <t> &arg, const A&... args) { for (int i = 0, sze = arg.size(); i < sze; ++i) cout << arg[i] << " \n"[i == sze - 1]; pt(args...); }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+//head
+constexpr int N = 2e5 + 10;
+int n, a[N];
+struct SEG {
+	struct node {
+		int Min, pos; 
+		node() { pos = 0; Min = 2e9; }
+		node operator + (const node &other) const {
+			node res = node();
+			if (Min < other.Min) {
+				res.Min = Min;
+				res.pos = pos;
+			} else {
+				res.Min = other.Min;
+				res.pos = other.pos;
+			}
+			return res;
+		}
+	}t[N << 2], res;
+	void build(int id, int l, int r) {
+		t[id] = node();
+		if (l == r) {
+			t[id].pos = l;
+			return;
+		}
+		int mid = (l + r) >> 1;
+		build(id << 1, l, mid);
+		build(id << 1 | 1, mid + 1, r);
+		t[id] = t[id << 1] + t[id << 1 | 1];
+	}
+	void update(int id, int l, int r, int pos, int v) {
+		if (l == r) {
+			t[id].Min = v;
+			return;
+		}
+		int mid = (l + r) >> 1;
+		if (pos <= mid) update(id << 1, l, mid, pos, v);
+		if (pos > mid) update(id << 1 | 1, mid + 1, r, pos, v);
+		t[id] = t[id << 1] + t[id << 1 | 1];
+	}
+	void query(int id, int l, int r, int ql, int qr) {
+		if (l >= ql && r <= qr) {
+			res = res + t[id];
+			return;
+		}
+		int mid = (l + r) >> 1;
+		if (ql <= mid) query(id << 1, l, mid, ql, qr);
+		if (qr > mid) query(id << 1 | 1, mid + 1, r, ql, qr);
+	}
+}seg;
+void run() {
+	n = rd();
+	for (int i = 1; i <= n; ++i) cin >> a[i];
+	seg.build(1, 1, n);
+	seg.update(1, 1, n, 1, a[1]);
+	int lazy = 1;
+	for (int i = 2; i <= n; ++i) {
+		seg.res = SEG::node();
+		seg.query(1, 1, n, 1, i - 1);
+		if (a[i] - 1 - lazy >= seg.res.Min) {
+			pt("YES");
+			pt(seg.res.pos, i);
+			return;
+		}
+		seg.update(1, 1, n, i, a[i] - lazy);
+		++lazy;
+	}
+	seg.build(1, 1, n);
+	seg.update(1, 1, n, n, a[n]);
+	lazy = 1;
+	for (int i = n - 1; i >= 1; --i) {
+		seg.res = SEG::node();
+		seg.query(1, 1, n, i + 1, n);
+		if (a[i] - 1 - lazy >= seg.res.Min) {
+			pt("YES");
+			pt(i, seg.res.pos);
+			return;
+		}
+		seg.update(1, 1, n, i, a[i] - lazy);
+		++lazy;
+	}
+	pt("NO");
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	int _T = rd();
+	while (_T--) run();
+	return 0;
+}
