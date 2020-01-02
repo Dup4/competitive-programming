b705f1e4334341129652602623bd79af29ef77f5
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-01-02 08:08:48 +0800

    up

---
+++ cf/1282/b1/b1.cpp
@@ -0,0 +1,67 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define endl "\n" 
+using namespace std;
+using db = double;
+using ll = long long;
+using ull = unsigned long long; 
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int rd() { int x; cin >> x; return x; }
+template <class T> inline void rd(T &x) { cin >> x; }
+template <class T> inline void rd(vector <T> &vec) { for (auto &it : vec) cin >> it; }  
+#define dbg(x...) do { cout << "\033[32;1m" << #x << " -> "; err(x); } while (0) 
+void err() { cout << "\033[39;0m" << endl; } 
+template <class T, class... Ts> void err(const T& arg, const Ts&... args) { cout << arg << ' '; err(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void err(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; err(args...); }
+void ptt() { cout << endl; }
+template <class T, class... Ts> void ptt(const T& arg, const Ts&... args) { cout << ' ' << arg; ptt(args...); }
+template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg; ptt(args...); }
+void pt() {}
+template <template<typename...> class T, typename t, typename... A>
+void pt(const T <t> &arg, const A&... args) { for (int i = 0, sze = arg.size(); i < sze; ++i) cout << arg[i] << " \n"[i == sze - 1]; pt(args...); }
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+inline ll qpow(ll base, ll n) { ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+//head
+constexpr int N = 2e5 + 10;
+int n, a[N]; ll P, K; 
+void run() {
+	cin >> n >> P >> K;
+	for (int i = 1; i <= n; ++i) cin >> a[i];
+	sort(a + 1, a + 1 + n);
+	vector <vector<int>> vec, S;
+	vec.clear(); vec.resize(K + 1);
+	S.clear(); S.resize(K + 1);
+	for (auto &it : vec) it.push_back(0);
+	for (auto &it : S) it.push_back(0);
+	for (int i = K; i <= n; ++i) {
+		vec[i % K].push_back(a[i]); 
+		S[i % K].push_back(S[i % K].back() + vec[i % K].back()); 
+	}
+	int res = 0;
+	int now = P; 
+	for (int i = 0; i < K; ++i) {
+		now -= a[i];
+		int pos = upper_bound(S[i].begin(), S[i].end(), now) - S[i].begin();
+		--pos; 
+		chmax(res, i + pos * K);
+	}
+	pt(res);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	int _T = rd();
+	while (_T--) run();
+	return 0;
+}
