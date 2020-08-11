6aaa079c37612143fedfe52dfab8987d729536d7
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-08-11 11:44:38 +0800

    up template

--- template/t.cpp
+++ template/t.cpp
@@ -32,18 +32,16 @@ void pt(const T <t> &arg, const A&... args) { for (int i = 0, sze = arg.size(); 
 inline ll qpow(ll base, ll n) { assert(n >= 0); ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
 //head
 constexpr int N = 1e5 + 10; 
-//int n; 
+int n; 
 
 void run() {
-	pt((new Solution())->)
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
 	cout << fixed << setprecision(20);
-	int _T = 1;
-	//nextInt();
+	int _T = nextInt();
 	while (_T--) run(); 
 //    for (int kase = 1; kase <= _T; ++kase) {
 //        cout << "Case #" << kase << ": ";
