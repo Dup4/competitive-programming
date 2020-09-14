258ee6835debc63ba3de235248f7b04ded5ac9ca
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-09-14 17:33:06 +0800

    up

--- buf/c.cpp
+++ buf/c.cpp
@@ -2,53 +2,13 @@
 using namespace std;
 using ll = long long;
 const int N = 1e5;
-vector <vector<int>> fac;
 
-int pri[N], check[N], mu[N];
-void sieve() {
-	memset(check, 0, sizeof check);
-	*pri = 0;
-	mu[1] = 1;
-	for (int i = 2; i < N; ++i) {
-		if (!check[i]) {
-			pri[++*pri] = i;
-			mu[i] = -1;
-		}
-		for (int j = 1; j <= *pri; ++j) {
-			if (1ll * i * pri[j] >= N) break;
-			check[i * pri[j]] = 1;
-			if (i % pri[j] == 0) {
-				mu[i * pri[j]] = 0;
-				break;
-			} else {
-				mu[i * pri[j]] = -mu[i];
-			}
-		}
-	}
-}
-
-ll f(int x) {
-	ll res = 0;
-	for (auto &it : fac[x]) {
-		res += 
-	}
-}
-
-ll g(int x) {
-
-}
 
 int main() {
-	sieve();
-	fac.clear();
-	fac.resize(N + 5);
-	for (int i = 1; i <= N; ++i) {
-		for (int j = i; j <= N; j += i) {
-			fac[j].push_back(i);
-		}
+	vector <int> vec({2, 3, 4, 5, 1});
+	next_permutation(vec.begin(), vec.end());
+	for (auto &it : vec) {
+		cout << it << endl;
 	}
-	for (int i = 1; i <= N; ++i) {
-		assert(f(i) == g(i));
-	}		
 	return 0;	
 }
