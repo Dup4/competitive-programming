a7b31d21526212dc791858ca56738ceb0581f62a
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-09-28 16:16:18 +0800

    add TI1050-Code-Repo

--- opentrains/10394/genc.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	int n = 12; cout << n << endl;
-	for (int i = 1; i <= n; ++i) printf("%d%c", i, " \n"[i == n]);
-	for (int i = 1; i <= n; ++i) printf("%d%c", i + 1, " \n"[i == n]);
-	return 0;
-}
--- source/19th_ZJU/g.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-while (t != 0) :
-	t -= 1
-	n = int(input())
-	res = 0
-	while (n > 0) : 
-		n /= 2
-		res += n
-	print res
